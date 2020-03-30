5917ae32c0c414d635239bbe623c45868882fad8
Kevin Hansen <kevin.hansen@weightmans.com>
2020-03-30 11:03:44 +0000

    added current balance
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C14378

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/CommercialRecoveries.NewLedgerView.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/CommercialRecoveries.NewLedgerView.sql
@@ -6,6 +6,7 @@ GO
 
 
 
+
 CREATE PROCEDURE [CommercialRecoveries].[NewLedgerView] --'M1001','29785'
 (
 @Client AS NVARCHAR(12)
@@ -18,12 +19,13 @@ SELECT MS_Prod.dbo.udCRLedgerSL.fileID
 ,clName AS [Client Name]
 ,fileNo AS [Matter]
 ,fileDesc AS [Matter Description]
+,curCurrentBal AS [Current Balance]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='2' THEN curOffice ELSE NULL END),0) AS [Recoverable Costs]
 ,ISNULL(SUM(CASE WHEN cboCatDesc IN ('1') THEN curOffice ELSE NULL END),0) AS [Recoverable Disbursements]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='0' THEN curOffice ELSE NULL END),0) AS [Unrecoverable Disbursements]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='7' THEN curOffice ELSE NULL END),0) AS [Unrecoverable Costs]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='4' THEN curOffice ELSE NULL END),0) AS [Interest]
-,ISNULL(SUM(CASE WHEN cboCatDesc='5' THEN curClient ELSE NULL END),0) AS [Payments]
+,ISNULL(SUM(CASE WHEN cboCatDesc='5' AND ISNULL(cboPayType,'') <>'PAY016' THEN curClient ELSE NULL END),0) AS [Payments]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='6' THEN curClient ELSE NULL END),0) AS [Receipta awaiting clearance]
 ,ISNULL(SUM(CASE WHEN cboCatDesc='3' THEN curOffice ELSE NULL END),0) AS [Original Balance]
 FROM [MS_PROD].dbo.udCRLedgerSL
@@ -31,12 +33,14 @@ INNER JOIN ms_prod.config.dbFile
  ON dbFile.fileID = udCRLedgerSL.fileID
 INNER JOIN ms_prod.config.dbClient
  ON dbClient.clID = dbFile.clID
+LEFT OUTER JOIN MS_Prod.dbo.udCRAccountInformation
+ ON udCRAccountInformation.fileID = dbFile.fileID
 WHERE clNo=@Client AND fileNo=@Matter
 GROUP BY udCRLedgerSL.fileID
 ,clNo 
 ,clName 
 ,fileNo 
 ,fileDesc 
-
+,curCurrentBal
 END
 GO
