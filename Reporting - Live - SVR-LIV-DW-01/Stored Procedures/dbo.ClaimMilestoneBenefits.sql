63763909f6aa6785d344a33bf93775e71624010a
Jamie Bonner <Jamie.Bonner@weightmans.com>
2020-09-08 16:50:14 +0000

    #71119 - added in last financial years figures
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C15164

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.ClaimMilestoneBenefits.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.ClaimMilestoneBenefits.sql
@@ -3,14 +3,9 @@ GO
 SET ANSI_NULLS ON
 GO
 
+-- Ticket #71119 - JB - added in Last FY figures
 
-
-
-
-
-
-
-CREATE PROCEDURE [dbo].[ClaimMilestoneBenefits] --'2021-02 (Jun-2020)','2021-03 (Jul-2020)'
+CREATE PROCEDURE [dbo].[ClaimMilestoneBenefits] --'2021-02 (Jun-2020)','2021-03 (Jul-2020)' 
 (
 @Period1 NVARCHAR(MAX)
 ,@Period2 NVARCHAR(MAX)
@@ -18,6 +13,7 @@ CREATE PROCEDURE [dbo].[ClaimMilestoneBenefits] --'2021-02 (Jun-2020)','2021-03 
 AS
 
 BEGIN
+
 --------------------------------------------------
 DECLARE @FinYear1 AS INT DECLARE @FinMonth1 AS INT
 
@@ -34,11 +30,14 @@ WHERE bill_fin_period=@Period2)
 
 SET @FinYear2=(SELECT DISTINCT  bill_fin_year FROM red_dw.dbo.dim_bill_date
 WHERE bill_fin_period=@Period2)
+
 ---------Period2 ---------------------------------------------------------------------------
 DECLARE @StartDate1 AS DATE
 DECLARE @StartDate2 AS DATE
+DECLARE @StartDate3 AS DATE
 DECLARE @EndDate1 AS DATE
 DECLARE @EndDate2 AS DATE
+DECLARE @EndDate3 AS DATE
 SET @StartDate1=(SELECT MIN(bill_date) FROM red_dw.dbo.dim_bill_date
 WHERE bill_fin_period=@Period1)
 SET @EndDate1=(SELECT MAX(bill_date) FROM red_dw.dbo.dim_bill_date
@@ -47,6 +46,10 @@ SET @StartDate2=(SELECT MIN(bill_date) FROM red_dw.dbo.dim_bill_date
 WHERE bill_fin_period=@Period2)
 SET @EndDate2=(SELECT MAX(bill_date) FROM red_dw.dbo.dim_bill_date
 WHERE bill_fin_period=@Period2)
+SET @StartDate3=(SELECT MIN(bill_date) FROM red_dw.dbo.dim_bill_date
+WHERE dim_bill_date.bill_fin_year=@FinYear2-1)
+SET @EndDate3=(SELECT MAX(bill_date) FROM red_dw.dbo.dim_bill_date
+WHERE dim_bill_date.bill_fin_year=@FinYear2-1)
 ------- Start and EndDates----------------------------------------------------------------------
 
 SELECT structure.Department
@@ -105,6 +108,18 @@ WHERE hierarchylevel2hist='Legal Ops - Claims'
 AND hierarchylevel4hist IS NOT NULL
 AND dss_current_flag='Y'
 --AND activeud=1
+
+UNION
+SELECT DISTINCT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+FROM red_dw.dbo.dim_matter_header_current
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
+WHERE hierarchylevel2hist='Legal Ops - Claims'
+AND hierarchylevel4hist IS NOT NULL
+AND dss_current_flag='Y'
+--AND activeud=1
 ) AS structure
 ----------------Hierarchy-------------------------------------------------
 LEFT OUTER JOIN (SELECT hierarchylevel3hist AS Department
@@ -119,6 +134,7 @@ INNER JOIN red_dw.dbo.dim_bill_date
 
 WHERE bill_fin_year= @FinYear1
 AND bill_fin_month_no=@FinMonth1
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
 GROUP BY  hierarchylevel3hist 
 ,hierarchylevel4hist
 UNION
@@ -134,6 +150,23 @@ INNER JOIN red_dw.dbo.dim_bill_date
 
 WHERE bill_fin_year= @FinYear2
 AND bill_fin_month_no=@FinMonth2
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
+GROUP BY  hierarchylevel3hist 
+,hierarchylevel4hist
+
+UNION
+SELECT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period],
+SUM(bill_amount) AS RevenueYTD
+FROM red_dw.dbo.fact_bill_activity
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_bill_activity.dim_fed_hierarchy_history_key
+INNER JOIN red_dw.dbo.dim_bill_date
+ ON dim_bill_date.bill_date = fact_bill_activity.bill_date
+
+WHERE bill_fin_year= @FinYear2-1
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
 GROUP BY  hierarchylevel3hist 
 ,hierarchylevel4hist
 ) AS Period1Revenue
@@ -178,6 +211,25 @@ AND dss_current_flag='Y'
 AND date_claim_concluded BETWEEN @StartDate2 AND @EndDate2
 GROUP BY hierarchylevel3hist 
 ,hierarchylevel4hist
+
+UNION
+SELECT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+,COUNT(1) AS NoConcluded
+,SUM(DATEDIFF(DAY,date_opened_case_management,date_claim_concluded)) AS DayToConclude
+FROM red_dw.dbo.dim_matter_header_current
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
+INNER JOIN red_dw.dbo.dim_detail_outcome
+ ON dim_detail_outcome.client_code = dim_matter_header_current.client_code
+ AND dim_detail_outcome.matter_number = dim_matter_header_current.matter_number
+WHERE hierarchylevel2hist='Legal Ops - Claims'
+AND hierarchylevel4hist IS NOT NULL
+AND dss_current_flag='Y'
+AND date_claim_concluded BETWEEN @StartDate3 AND @EndDate3
+GROUP BY hierarchylevel3hist 
+,hierarchylevel4hist
 ) AS Concluded
  ON Concluded.Department = structure.Department
  AND Concluded.Team = structure.Team
@@ -218,6 +270,25 @@ WHERE hierarchylevel2hist='Legal Ops - Claims'
 AND hierarchylevel4hist IS NOT NULL
 AND dss_current_flag='Y'
 AND date_claim_concluded BETWEEN @StartDate2 AND @EndDate2
+GROUP BY hierarchylevel3hist 
+,hierarchylevel4hist
+
+UNION
+SELECT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+,COUNT(1) AS NoSettled
+,SUM(DATEDIFF(DAY,date_opened_case_management,date_costs_settled)) AS DayToSettle
+FROM red_dw.dbo.dim_matter_header_current
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
+INNER JOIN red_dw.dbo.dim_detail_outcome
+ ON dim_detail_outcome.client_code = dim_matter_header_current.client_code
+ AND dim_detail_outcome.matter_number = dim_matter_header_current.matter_number
+WHERE hierarchylevel2hist='Legal Ops - Claims'
+AND hierarchylevel4hist IS NOT NULL
+AND dss_current_flag='Y'
+AND date_claim_concluded BETWEEN @StartDate3 AND @EndDate3
 GROUP BY hierarchylevel3hist 
 ,hierarchylevel4hist
  ) AS Settled
@@ -277,6 +348,32 @@ AND hierarchylevel4hist IS NOT NULL
 AND dss_current_flag='Y'
 GROUP BY hierarchylevel3hist 
 ,hierarchylevel4hist
+
+UNION
+SELECT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+,SUM(CASE WHEN Milestones.Completed>=1 THEN 1 ELSE 0 END)  AS [WizardCompleted]
+,SUM(CASE WHEN Milestones.Completed=0 THEN 1 ELSE 0 END)  AS [WizardIncomplete]
+FROM red_dw.dbo.dim_matter_header_current
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
+LEFT OUTER JOIN (SELECT fileID,SUM(CASE WHEN tskComplete=1 THEN 1 ELSE 0 END) AS Completed
+,SUM(CASE WHEN tskComplete=0 THEN 1 ELSE 0 END) AS Incompleted
+,MAX(tskCompleted) AS [DateLastCompleted]
+FROM MS_PROD.dbo.dbTasks
+WHERE tskType='MILESTONE'
+AND tskDesc LIKE '%Stage%' AND tskDesc LIKE '%Wizard%'
+AND tskactive=1
+AND CONVERT(DATE,tskCompleted,103) BETWEEN @StartDate3 AND @EndDate3
+GROUP BY fileID) AS Milestones
+ ON ms_fileid=Milestones.fileID
+
+WHERE hierarchylevel2hist='Legal Ops - Claims'
+AND hierarchylevel4hist IS NOT NULL
+AND dss_current_flag='Y'
+GROUP BY hierarchylevel3hist 
+,hierarchylevel4hist
  ) AS Wizards
   ON Wizards.Department = structure.Department
  AND Wizards.Team = structure.Team
@@ -297,6 +394,7 @@ INNER JOIN red_dw.dbo.dim_matter_header_current ON fact_write_off.dim_matter_hea
        = dim_matter_header_current.dim_matter_header_curr_key
 INNER JOIN red_dw.dbo.dim_date ON dim_date.dim_date_key=fact_write_off.dim_write_off_date_key
 WHERE fin_period=@Period1
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
 GROUP BY dim_fed_hierarchy_history.hierarchylevel3hist
 ,dim_fed_hierarchy_history.hierarchylevel4hist
 UNION
@@ -313,6 +411,25 @@ INNER JOIN red_dw.dbo.dim_matter_header_current ON fact_write_off.dim_matter_hea
        = dim_matter_header_current.dim_matter_header_curr_key
 INNER JOIN red_dw.dbo.dim_date ON dim_date.dim_date_key=fact_write_off.dim_write_off_date_key
 WHERE fin_period=@Period2
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
+GROUP BY dim_fed_hierarchy_history.hierarchylevel3hist
+,dim_fed_hierarchy_history.hierarchylevel4hist
+
+UNION
+SELECT dim_fed_hierarchy_history.hierarchylevel3hist AS Department
+,dim_fed_hierarchy_history.hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+,SUM(red_dw.dbo.fact_write_off.write_off_amt) AS WriteOff
+FROM red_dw.dbo.fact_write_off
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history 
+       ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_write_off.dim_fed_matter_owner_key
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history  feeearner
+       ON feeearner.dim_fed_hierarchy_history_key = fact_write_off.dim_fed_hierarchy_history_key
+INNER JOIN red_dw.dbo.dim_matter_header_current ON fact_write_off.dim_matter_header_curr_key
+       = dim_matter_header_current.dim_matter_header_curr_key
+INNER JOIN red_dw.dbo.dim_date ON dim_date.dim_date_key=fact_write_off.dim_write_off_date_key
+WHERE dim_date.fin_year = @FinYear2-1
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
 GROUP BY dim_fed_hierarchy_history.hierarchylevel3hist
 ,dim_fed_hierarchy_history.hierarchylevel4hist
 ) AS WriteOffs
@@ -355,6 +472,26 @@ LEFT OUTER JOIN red_dw.dbo.fact_budget_activity
   ON fact_budget_activity.dim_fed_hierarchy_history_key = dim_fed_hierarchy_history.dim_fed_hierarchy_history_key
   AND fact_budget_activity.dim_budget_date_key = fact_agg_billable_time_monthly_rollup.dim_gl_date_key
 WHERE gl_fin_period = @Period2
+AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
+AND hierarchylevel4hist IS NOT NULL
+GROUP BY hierarchylevel3hist,
+         hierarchylevel4hist
+
+UNION
+SELECT hierarchylevel3hist AS Department
+,hierarchylevel4hist AS [Team]
+,'Last FY' AS [Period]
+,CAST(SUM(minutes_recorded / 60) AS DECIMAL(10, 1)) AS ChargeableHrs
+,SUM(fact_budget_activity.contracted_hours_in_month)  AS ContractedHrs
+FROM red_dw.dbo.fact_agg_billable_time_monthly_rollup
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history 
+  ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_agg_billable_time_monthly_rollup.dim_fed_hierarchy_history_key
+INNER JOIN red_dw.dbo.dim_gl_date 
+  ON dim_gl_date.dim_gl_date_key = fact_agg_billable_time_monthly_rollup.dim_gl_date_key
+LEFT OUTER JOIN red_dw.dbo.fact_budget_activity 
+  ON fact_budget_activity.dim_fed_hierarchy_history_key = dim_fed_hierarchy_history.dim_fed_hierarchy_history_key
+  AND fact_budget_activity.dim_budget_date_key = fact_agg_billable_time_monthly_rollup.dim_gl_date_key
+WHERE dim_gl_date.gl_fin_year = @FinYear2-1
 AND dim_fed_hierarchy_history.hierarchylevel2hist = 'Legal Ops - Claims'
 AND hierarchylevel4hist IS NOT NULL
 GROUP BY hierarchylevel3hist,
