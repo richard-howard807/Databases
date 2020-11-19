656cc5a8c2aa52c5c5b9b314815b7e0438ac077d
Jamie Bonner <Jamie.Bonner@weightmans.com>
2020-11-19 19:31:18 +0000

    #79133 - New report to replace billing macro Motor currently use, shows when matters can be billed
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C15540

--- Reporting - Live - SVR-LIV-DW-01/Tables/dbo.client_billing_sla.sql
+++ Reporting - Live - SVR-LIV-DW-01/Tables/dbo.client_billing_sla.sql
@@ -8,6 +8,7 @@ CREATE TABLE [dbo].[client_billing_sla]
 [wip_minimum] [float] NULL,
 [bill_frequency_months] [float] NULL,
 [on_weightmans_quarter] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
-[bill_rule_num] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL
+[bill_rule_num] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[fixed_fee_success_fee] [nvarchar] (3) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
