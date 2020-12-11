397823ab7ac15df24be8b1532c9b98afc147e9f8
6237 <6237@sbc.tfs.local>
2020-12-11 14:23:08 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C15680

--- red_dw - SVR-LIV-DW-01/Database Triggers/ddl_trigger_table_index_create.sql
+++ red_dw - SVR-LIV-DW-01/Database Triggers/ddl_trigger_table_index_create.sql
@@ -58,5 +58,5 @@ ENABLE TRIGGER ddl_trigger_table_index_create ON DATABASE
 GO
 ENABLE TRIGGER ddl_trigger_table_index_create ON DATABASE
 GO
-DISABLE TRIGGER ddl_trigger_table_index_create ON DATABASE
+ENABLE TRIGGER ddl_trigger_table_index_create ON DATABASE
 GO
