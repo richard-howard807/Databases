b8cfb080dc223ef3f7b21482d4cd44c44d0226d5
Jamie Bonner <Jamie.Bonner@weightmans.com>
2021-02-18 12:08:45 +0000

    #88127 New MSG report
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C15985

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.markerstudy_savings_report.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.markerstudy_savings_report.sql
@@ -19,11 +19,13 @@ SELECT
 	, dim_matter_header_current.master_client_code + '/' + dim_matter_header_current.master_matter_number		AS [Panel Firm Ref.]
 	, CAST(dim_detail_core_details.date_instructions_received AS DATE)			AS [Date Instructed]
 	, fact_detail_paid_detail.interim_damages_paid_by_client_preinstruction		AS [Paid]
-	, ''			AS [Outstanding]  --New field to be added
-	, ''			AS [Recoveries Received]	--New field to be added
-	, ''			AS [Recoveries Reserved]	--New field to be added
-	--, (ISNULL(fact_detail_paid_detail.interim_damages_paid_by_client_preinstruction, 0) + ISNULL([Outstanding], 0)) 
-	--		- (ISNULL([Recoveries Received], 0) + ISNULL([Recoveries Reserved], 0))			AS [Incurred (Net of Recoveries)]  --To be updated when new fields available
+	, ''			AS [Outstanding]  --curCliOutRes
+	, ''			AS [Recoveries Received]	--curCliRecMade
+	, ''			AS [Recoveries Reserved]	--curCliRecRes
+	/*
+	, (ISNULL(fact_detail_paid_detail.interim_damages_paid_by_client_preinstruction, 0) + ISNULL([Outstanding], 0)) 
+			- (ISNULL([Recoveries Received], 0) + ISNULL([Recoveries Reserved], 0))			AS [Incurred (Net of Recoveries)]  --To be updated when new fields available
+	*/
 	, CASE	
 		WHEN dim_detail_outcome.outcome_of_case IS NULL THEN 
 			'No'
@@ -32,12 +34,12 @@ SELECT
 	  END						AS [Damages Settled]
 	, fact_finance_summary.damages_paid			AS [Damages Settlement Amount]
 	, CAST(dim_detail_outcome.date_claim_concluded AS DATE)					AS [Date Damages Settled]
-	, ''						AS [Defendant Offer Made]   --New field to be added
-	, ''						AS [Date Defendant Offer Made]		--New field to be added
-	, ''						AS [Type of Defendant Offer]		--New field to be added
-	, ''						AS [Claimant Offer Made]		--New field to be added
-	, ''						AS [Date Claimant Offer Made]	--New field to be added
-	, ''						AS [Type of Claimant Offer]		--New field to be added
+	, ''						AS [Defendant Offer Made]   --udMIClientMSGDefDamSL.curAmount
+	, ''						AS [Date Defendant Offer Made]		--udMIClientMSGDefDamSL.dteOffer
+	, ''						AS [Type of Defendant Offer]		--udMIClientMSGDefDamSL.cboTypeOffer 
+	, ''						AS [Claimant Offer Made]		--udMIClientMSGClaimDamSL.curAmount
+	, ''						AS [Date Claimant Offer Made]	--udMIClientMSGClaimDamSL.dteOffer
+	, ''						AS [Type of Claimant Offer]		--udMIClientMSGClaimDamSL.cboTypeOffer 
 	, CASE	
 		WHEN dim_detail_outcome.date_costs_settled IS NULL THEN
 			'No'
@@ -47,10 +49,17 @@ SELECT
 	, fact_finance_summary.claimants_total_costs_claimed		AS [Costs Claimed]
 	, fact_finance_summary.claimants_costs_paid			AS [Costs Settlement Amount]
 	, CAST(dim_detail_outcome.date_costs_settled AS DATE)		AS [Date Costs Agreed]
-	, ''				AS [Offer Made by Defendant to Settle Claimant Costs]		--New field to be added
-	, ''				AS [Date Costs Offer Made]	--New field to be added
+	, ''				AS [Offer Made by Defendant to Settle Claimant Costs]		--udMIClientMSGDefCostsSL.curAmount 
+	, ''				AS [Date Costs Offer Made]									--udMIClientMSGDefCostsSL.dteOffer 
 	, dim_detail_core_details.proceedings_issued			AS [Proceedings Issued]
-	, ''				AS [Reserve Recommendation Made]	--If there's a difference between out total reserve and (paid + outstanding columns) then Yes else No
+	/*
+	, CASE	
+		WHEN ISNULL(fact_detail_reserve_detail.total_reserve, 0) = (ISNULL(fact_detail_paid_detail.interim_damages_paid_by_client_preinstruction, 0) + ISNULL([Outstanding], 0)) THEN
+			'No'
+		ELSE
+			'Yes'
+	  END					AS [Reserve Recommendation Made]	--If there's a difference between out total reserve and (paid + outstanding columns) then Yes else No
+	*/
 	, CAST(dim_matter_header_current.date_closed_practice_management AS DATE)		AS [Date File Closed]
 	, ''				AS [Relevant Comments (Optional)] 
 	--======================================================================================================
@@ -63,7 +72,7 @@ SELECT
 	, dim_detail_core_details.track				AS [Track]
 	, dim_client_involvement.insurerclient_name		AS [Insurer Associate]
 	, dim_detail_core_details.present_position			AS [Present Position]
-	, ''				AS [Claims Strategy]  --New field to be added
+	, ''				AS [Claims Strategy]  --cboInsTypeMSG
 	, dim_detail_core_details.injury_type		AS [Injury Type]
 	, fact_detail_paid_detail.damages_interims		AS [Interim Damages Post Instruction]
 	, dim_detail_core_details.suspicion_of_fraud			AS [Suspicion of Fraud]
@@ -97,12 +106,13 @@ WHERE 1 = 1
 	AND dim_matter_header_current.master_matter_number <> 0
 	AND dim_matter_header_current.reporting_exclusions = 0
 	AND LOWER(LTRIM(RTRIM(ISNULL(dim_detail_outcome.outcome_of_case, '')))) <> 'exclude from reports'
-	--There will need to be a filter for the new instruction type field (cboInsTypeMSG) to filter to only Markerstudy Project. 
+	/*
+	AND cboInsTypeMSG = 'MSG Savings project' --Need to update the field to dwh field
+	*/
 ORDER BY	
 	dim_detail_core_details.date_instructions_received
 	, dim_matter_header_current.master_matter_number
 
 
 END	
-
 GO
