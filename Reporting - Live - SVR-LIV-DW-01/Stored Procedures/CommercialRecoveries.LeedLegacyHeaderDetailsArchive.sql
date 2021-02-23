586cc04cda7ff88c03dc2cf5c9c666d555facc07
Kevin Hansen <kevin.hansen@weightmans.com>
2021-02-23 12:46:35 +0000

    added in archive number
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16027

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/CommercialRecoveries.LeedLegacyHeaderDetailsArchive.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/CommercialRecoveries.LeedLegacyHeaderDetailsArchive.sql
@@ -9,6 +9,7 @@ GO
 
 
 
+
 CREATE PROCEDURE [CommercialRecoveries].[LeedLegacyHeaderDetailsArchive]
 (
 @SourceSystemID AS NVARCHAR(100)
@@ -24,6 +25,7 @@ SELECT  RTRIM(CAST(arclin AS NVARCHAR(20))) + '-' + RTRIM(CAST(armatn AS NVARCHA
 ,arfile.ardtds AS [DestructionDate]
 ,arfile.arloct AS [Archivelocation]
 ,'\\svr-liv-fs-09\Arcdocs\' + TRIM(CAST(arclin AS NVARCHAR(5))) + '\' +TRIM(CAST(armatn AS NVARCHAR(5)))+'\pms\pms_archive.rtf' AS MatterDocs
+,CAST(arfile.arnumb AS NVARCHAR(MAX)) AS [ArchiveNumber]
 FROM [SVR-LIV-SQL-04\LEGACYREADONLY].fwact.dbo.arfile AS arfile
 --LEFT OUTER JOIN [SVR-LIV-SQL-04\LEGACYREADONLY].[fwact].[dbo].[FEFILE] AS FE
 -- ON arfile.areact=FE.feidnm
