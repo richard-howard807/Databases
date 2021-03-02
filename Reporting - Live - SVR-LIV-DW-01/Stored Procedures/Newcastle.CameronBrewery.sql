ec37d631f1ba5c9dd80a637a8d4a0e4cb868c22f
Kevin Hansen <kevin.hansen@weightmans.com>
2021-03-02 14:15:16 +0000

    replaced the old reference with current Mattersphere client and matter
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16093

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/Newcastle.CameronBrewery.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/Newcastle.CameronBrewery.sql
@@ -3,9 +3,6 @@ GO
 SET ANSI_NULLS ON
 GO
 
-
-
-
 CREATE PROCEDURE [Newcastle].[CameronBrewery]
 (
 @StartDate AS DATE
@@ -15,16 +12,16 @@ CREATE PROCEDURE [Newcastle].[CameronBrewery]
 AS 
 BEGIN
 
-SELECT  ISNULL(REPLACE(CRSystemSourceID,'-','.'),clNo+ '.'+fileNo) AS [File Number]
+SELECT  clNo+ '.'+fileNo AS [File Number]
 ,fileDesc AS [Matter Description]
 ,curRevEstimate AS [Agreed Fee]
 ,Financials.FeesBilledToDate AS [Fees Billed to Date]
 ,Financials.MonthFees AS [Monthly Fees]
 ,ISNULL(curRevEstimate,0) - Financials.FeesBilledToDate AS [Fees to be Billed]
-FROM MS_PROD.config.dbFile
-INNER JOIN MS_PROD.config.dbClient
+FROM MS_PROD.config.dbFile WITH(NOLOCK)
+INNER JOIN MS_PROD.config.dbClient WITH(NOLOCK)
  ON dbClient.clID = dbFile.clID
-LEFT OUTER JOIN MS_PROD.dbo.udExtFile
+LEFT OUTER JOIN MS_PROD.dbo.udExtFile WITH(NOLOCK)
  ON udExtFile.fileID = dbFile.fileID
 LEFT OUTER JOIN (SELECT MattIndex,SUM(OrgFee) AS FeesBilledToDate
 ,SUM(CASE WHEN InvDate BETWEEN @StartDate AND @EndDate THEN OrgFee ELSE NULL END) AS MonthFees
@@ -33,7 +30,7 @@ INNER JOIN TE_3E_Prod.dbo.Matter WITH(NOLOCK)
  ON LeadMatter=MattIndex
  GROUP BY MattIndex) AS Financials
   ON fileExtLinkID=MattIndex
-LEFT OUTER JOIN MS_PROD.dbo.udMICoreGeneral
+LEFT OUTER JOIN MS_PROD.dbo.udMICoreGeneral WITH(NOLOCK)
  ON udMICoreGeneral.fileID = udExtFile.fileID
 WHERE clNo IN ('W22555','W24107')
 --AND fileNo<>'1'
