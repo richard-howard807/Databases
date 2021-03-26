3dec305ad78746ada367c76b5e253ee0f36ba072
Jamie Bonner <Jamie.Bonner@weightmans.com>
2021-03-26 16:21:01 +0000

    Ticket #93737: Added columns as per ticket
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16245

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.riverside_monthly_report.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.riverside_monthly_report.sql
@@ -4,11 +4,13 @@ SET ANSI_NULLS ON
 GO
 
 /*
--- =============================================
--- Author:		Jamie Bonner
--- Create date: 2021-03-12
--- Description: #91818 New report request for client Riverside
--- =============================================
+=============================================
+Author:			Jamie Bonner
+Create date:	2021-03-12
+Description:	#91818 New report request for client Riverside
+=============================================
+Ticket #93737: Added columns as per ticket
+=============================================
 */
 
 CREATE PROCEDURE [dbo].[riverside_monthly_report]
@@ -67,6 +69,30 @@ SELECT
 	, fact_detail_property.tenants_solicitors_costs				AS [3rd Party Solicitor Fees Paid (Incl. VAT)]
 	, CAST(dim_matter_header_current.date_closed_practice_management AS DATE)			AS [Case Closed Date]
 	, dim_detail_property.status_comment			AS [Reason Case Length Exceeds 6 Months]
+	, dim_matter_worktype.work_type_name			AS [Work Type]
+	, CAST(dim_detail_claim.gascomp_lba_expiry_date AS DATE)		AS [Expiry Date of Gas Safety Certificate]
+	, CAST(dim_detail_claim.gascomp_lba_date_upload AS DATE)		AS [Letter Before Action Sent Date]
+	, CAST(dim_detail_claim.gascomp_injunction_application_date AS DATE)		AS [Injunction Application Filed at Court Date]
+	, CAST(dim_detail_claim.gascomp_injunction_service_date AS DATE)		AS [Injunction Served on Tenant]
+	, CAST(dim_detail_claim.gascomp_hearing_date AS DATE)			AS [Court Hearing Date]
+	, CAST(dim_detail_claim.gascomp_date_access_obtained AS DATE)	AS [Access Obtained]
+	, CASE
+		WHEN LOWER(te_3e_disb.Description) = 'process_server_fees' THEN
+			te_3e_disb.disb_value
+		ELSE 
+			0
+	  END				AS [Process Server Fees]
+	, ''	AS [Reason Case Length Exceeds 3 Months]
+	, CASE 
+		WHEN dim_matter_worktype.work_type_code = '1150' THEN --Landlord & Tenant - Disrepair
+			1
+		WHEN dim_matter_worktype.work_type_code = '1002' THEN --Landlord & Tenant - Residential
+			2
+		WHEN dim_matter_worktype.work_type_code = '1105' THEN --Social Housing - Property
+			3
+		WHEN dim_matter_worktype.work_type_code = '1011' THEN --Injunction
+			4
+	  END						AS tab_order			
 FROM red_dw.dbo.dim_matter_header_current
 	INNER JOIN red_dw.dbo.dim_matter_worktype
 		ON dim_matter_worktype.dim_matter_worktype_key = dim_matter_header_current.dim_matter_worktype_key
@@ -85,14 +111,23 @@ FROM red_dw.dbo.dim_matter_header_current
 	LEFT OUTER JOIN red_dw.dbo.dim_detail_core_details
 		ON dim_detail_core_details.client_code = dim_matter_header_current.client_code
 			AND dim_detail_core_details.matter_number = dim_matter_header_current.matter_number
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_claim
+		ON dim_detail_claim.client_code = dim_matter_header_current.client_code
+			AND dim_detail_claim.matter_number = dim_matter_header_current.matter_number
 	LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
 		ON fact_finance_summary.client_code = dim_matter_header_current.client_code
 			AND fact_finance_summary.matter_number = dim_matter_header_current.matter_number
 	LEFT OUTER JOIN (
 						SELECT DISTINCT
 							Matter.Number
-							, CostType.Description
+							, CASE
+								WHEN LOWER(CostCard.Narrative) LIKE '%process serv%' THEN
+									'process_server_fees'
+								ELSE
+									CostType.Description
+							  END			AS [Description]
 							, SUM(CostCard.WorkAmt)		AS disb_value	
+						--SELECT CostCard.*
 						FROM TE_3E_Prod.dbo.Matter WITH (NOLOCK)
 							INNER JOIN red_dw.dbo.dim_matter_header_current 
 								ON dim_matter_header_current.master_client_code + '-' + dim_matter_header_current.master_matter_number  = Matter.Number COLLATE DATABASE_DEFAULT 
@@ -106,19 +141,25 @@ FROM red_dw.dbo.dim_matter_header_current
 								ON Voucher.VchrIndex = CostCard.Voucher
 						WHERE 1 = 1
 							AND dim_matter_header_current.master_client_code = 'W15603'
-							AND dim_matter_worktype.work_type_code = '1150'
+							AND dim_matter_worktype.work_type_code IN ('1002', '1011', '1105', '1150')
 						GROUP BY
 							Matter.Number
-							, CostType.Description
-					) AS te_3e_disb
+							, CASE
+								WHEN LOWER(CostCard.Narrative) LIKE '%process serv%' THEN
+									'process_server_fees'
+								ELSE
+									CostType.Description
+							  END)							AS te_3e_disb
 			ON te_3e_disb.Number COLLATE DATABASE_DEFAULT = dim_matter_header_current.master_client_code + '-' + dim_matter_header_current.master_matter_number
 WHERE 1 = 1
 	AND dim_matter_header_current.master_client_code = 'W15603'
 	AND dim_matter_header_current.reporting_exclusions = 0
-	AND dim_matter_worktype.work_type_code = '1150'
+	AND dim_matter_worktype.work_type_code IN ('1002', '1011', '1105', '1150')
 ORDER BY
-	[Date Opened]
+	tab_order
+	, [Date Opened]
 
 END
+
 
 GO
