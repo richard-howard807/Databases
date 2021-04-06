c76e9f6604ce2b952a8856ad34ce12dc05497835
Jamie Bonner <Jamie.Bonner@weightmans.com>
2021-04-06 11:34:57 +0000

    #94653 - changed agreed fee column to look at fact_finance_summary.fixed_fee_amount
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16271

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/Newcastle.CameronBrewery.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/Newcastle.CameronBrewery.sql
@@ -8,13 +8,18 @@ CREATE PROCEDURE [Newcastle].[CameronBrewery]
 @StartDate AS DATE
 ,@EndDate AS DATE
 )
+AS
 
-AS 
+-- For testing
+--DECLARE @StartDate AS DATE = CONVERT(DATE,DATEADD(mm, DATEDIFF(mm, 0, GETDATE()) - 1, 0),103) 
+--		, @EndDate AS DATE = CONVERT(DATE,DATEADD(DAY, -(DAY(GETDATE())), GETDATE()),103)
+
 BEGIN
 
 SELECT  clNo+ '.'+fileNo AS [File Number]
 ,fileDesc AS [Matter Description]
-,curRevEstimate AS [Agreed Fee]
+--,curRevEstimate AS [Agreed Fee]
+, fact_finance_summary.fixed_fee_amount AS [Agreed Fee]
 ,Financials.FeesBilledToDate AS [Fees Billed to Date]
 ,Financials.MonthFees AS [Monthly Fees]
 ,ISNULL(curRevEstimate,0) - Financials.FeesBilledToDate AS [Fees to be Billed]
@@ -32,6 +37,11 @@ INNER JOIN TE_3E_Prod.dbo.Matter WITH(NOLOCK)
   ON fileExtLinkID=MattIndex
 LEFT OUTER JOIN MS_PROD.dbo.udMICoreGeneral WITH(NOLOCK)
  ON udMICoreGeneral.fileID = udExtFile.fileID
+INNER JOIN red_dw.dbo.dim_matter_header_current
+	ON dim_matter_header_current.ms_fileid = dbFile.fileID
+LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
+	ON fact_finance_summary.client_code = dim_matter_header_current.client_code
+		AND fact_finance_summary.matter_number = dim_matter_header_current.matter_number
 WHERE clNo IN ('W22555','W24107')
 --AND fileNo<>'1'
 AND fileNo<>'0'
