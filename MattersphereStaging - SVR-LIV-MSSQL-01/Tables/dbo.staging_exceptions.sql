c7941e6cdbb11063da1033c6ffa825a3bf098047
6237 <6237@sbc.tfs.local>
2021-04-16 12:19:50 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16315

--- MattersphereStaging - SVR-LIV-MSSQL-01/Tables/dbo.staging_exceptions.sql
+++ MattersphereStaging - SVR-LIV-MSSQL-01/Tables/dbo.staging_exceptions.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[staging_exceptions]
 (
 [ms_fileid] [bigint] NULL,
+[exceptionruleid] [int] NULL,
 [fieldname] [varchar] (200) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
