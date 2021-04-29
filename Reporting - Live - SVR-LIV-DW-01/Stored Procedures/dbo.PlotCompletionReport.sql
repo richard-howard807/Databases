609dbd6d67fa2bc8aca08a32366faacfc520db74
6237 <6237@sbc.tfs.local>
2021-04-29 13:58:59 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16368

---
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.PlotCompletionReport.sql
@@ -0,0 +1,75 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[PlotCompletionReport]
+(
+@StartDate AS DATE
+,@EndDate AS DATE
+)
+AS 
+
+BEGIN 
+
+SELECT
+master_client_code + '-' + master_matter_number AS [Client & Matter Reference]
+,name AS [Fee Earner Name]
+,txtPlotNo AS [Plot Number]
+,COALESCE(david_wilson_homes_limited_developments
+,barratt_manchester_developments
+,bardsley_development
+,thomas_jones_sons_limited_development
+,persimmon_homes_limited_development
+,manchester_ship_canal_developments_advent_limited
+,greenfields_place_development_company_limited_developments) AS [Development Name]
+,CASE WHEN cboFundRec ='Y' THEN 'Yes' WHEN cboFundRec='N' THEN 'No' ELSE cboFundRec END AS [Funds received]
+,curProcVal AS [Proceeds of Sale amount]
+,curLegalFeePS AS [Legal Fee]
+,curTTFee AS [TT Fee]
+,curAdminFeePS AS [Admin Fee]
+,curOtherDisbPS AS [Other Disbursements]
+,NULL  AS [Addressee – Invoice 1]
+,curDocFeePS AS [Document Fee]
+,txtPurchaser1Fullname AS [Document Fee Paid by]
+,curPartExFee AS [Part Exchange Legal Fee]
+,curPartExDisb AS [Part Exchange Other Disbursement]
+,NULL AS [Part Exchange File Reference]
+,mtxtAddressPartEx   AS [Part Exchange Property Address]
+,NULL AS [Builder Account Details]
+,txtSortCodePS AS [Sort Code]
+,txtAccNumberPS AS [Account Number]
+,txtAccRefPS AS [Account Reference]
+,CASE WHEN cboFinInstruct ='Y' THEN 'Yes' WHEN cboFinInstruct='N' THEN 'No' ELSE cboFinInstruct END  AS [Finance Instructed]
+,completion_date AS [Completion Date]
+,dim_detail_plot_details.exchange_date AS [Exchange Date]
+--,cboHasRetRec AS [Has retention undertaking been received?”]
+--,cboRecCTD AS [Have we received CTD – Confirmation to Developer]
+--,cboRecWarrNot AS [Have we received Warranty cover note]
+--,cboH2BRec AS [Help to Buy Undertaking received]
+--,cboHoldLease AS [Holding signed original lease or transfer on file]
+--,cboCompSign AS [Is Exchange signed off]
+
+
+
+FROM red_dw.dbo.dim_matter_header_current
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history
+ ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT AND dss_current_flag='Y'
+INNER JOIN red_dw.dbo.dim_detail_property
+ ON dim_detail_property.client_code = dim_matter_header_current.client_code
+ AND dim_detail_property.matter_number = dim_matter_header_current.matter_number
+LEFT OUTER JOIN red_dw.dbo.dim_detail_plot_details
+ ON dim_detail_plot_details.client_code = dim_matter_header_current.client_code
+ AND dim_detail_plot_details.matter_number = dim_matter_header_current.matter_number 
+LEFT OUTER JOIN MS_Prod.dbo.udPlotSalesFileOpeningTF
+ ON ms_fileid=udPlotSalesFileOpeningTF.fileID
+LEFT OUTER JOIN MS_Prod.dbo.udPlotSalesPreExc
+ ON ms_fileid=udPlotSalesPreExc.fileID
+LEFT OUTER JOIN ms_prod.dbo.udPlotSalesExchange
+ ON ms_fileid=udPlotSalesExchange.fileID
+ 
+WHERE  CONVERT(DATE,completion_date,103) BETWEEN @StartDate AND @EndDate
+AND cboFundRec ='Y'
+AND cboFinInstruct='N'
+
+END
+GO
