307a1db6d25b564de73222ffc6aa56525e2f4f4b
6237 <6237@sbc.tfs.local>
2021-05-04 07:53:05 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16378

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.SupervisionMI.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.SupervisionMI.sql
@@ -12,15 +12,16 @@ Current Version:	Initial Create
 ====================================================
  */
 
-CREATE PROCEDURE [dbo].[SupervisionMI]	--EXEC [dbo].[SupervisionMI] 'Legal Ops - Claims', 'Disease','Disease Liverpool 2','Liverpool','Open'
-
+CREATE PROCEDURE [dbo].[SupervisionMI]	--EXEC [dbo].[SupervisionMI] 'Legal Ops - Claims', 'Disease','Disease Liverpool 2','Open','Giselle Drouillard'
+											  
 (
 @Division AS VARCHAR(MAX),
 @Department AS VARCHAR(MAX),
-@Team AS VARCHAR(MAX)
+@Team AS VARCHAR(MAX),
 --@Location AS VARCHAR(MAX),
---@Status AS VARCHAR(MAX)	
---@Client AS VARCHAR(max)
+@Status AS VARCHAR(MAX)	,
+--@Client AS VARCHAR(max),
+@FeeEarner AS VARCHAR(MAX)
 ) 
 AS
 BEGIN 
@@ -43,16 +44,16 @@ SELECT ListValue  INTO #Department FROM 	dbo.udt_TallySplit(',', @Department)
 IF OBJECT_ID('tempdb..#Team') IS NOT NULL   DROP TABLE #Team
 SELECT ListValue  INTO #Team FROM 	dbo.udt_TallySplit(',', @Team)
 
---IF OBJECT_ID('tempdb..#Status') IS NOT NULL   DROP TABLE #Status
---SELECT ListValue  INTO #Status FROM 	dbo.udt_TallySplit(',', @Status)
+IF OBJECT_ID('tempdb..#Status') IS NOT NULL   DROP TABLE #Status
+SELECT ListValue  INTO #Status FROM 	dbo.udt_TallySplit(',', @Status)
 
---IF OBJECT_ID('tempdb..#FeeEarner') IS NOT NULL   DROP TABLE #FeeEarner
---SELECT ListValue  INTO #FeeEarner FROM 	dbo.udt_TallySplit('|', @FeeEarner)
+IF OBJECT_ID('tempdb..#FeeEarner') IS NOT NULL   DROP TABLE #FeeEarner
+SELECT ListValue  INTO #FeeEarner FROM 	dbo.udt_TallySplit(',', @FeeEarner)
 
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 SELECT dim_matter_header_current.master_client_code +'-'+master_matter_number AS [MattersphereRef]
 ,red_dw.dbo.dim_detail_claim.name_of_reviewer	 AS [Name of reviewer]
-,red_dw.dbo.dim_detail_claim.name_of_future_reviewer AS [Next review to be completed by]
+,red_dw.dbo.dim_detail_claim.name_of_future_reviewer	 AS [Next review to be completed by]
 ,red_dw.dbo.dim_detail_claim.date_most_recent_supervisor_review	 AS [Date of review]
 ,red_dw.dbo.dim_detail_claim.date_case_status_decision_made AS [Date case status decision made]
 ,red_dw.dbo.dim_detail_claim.date_of_next_review   AS [Date of next review]
@@ -61,6 +62,7 @@ SELECT dim_matter_header_current.master_client_code +'-'+master_matter_number AS
 ,hierarchylevel3hist AS [Department]
 ,hierarchylevel4hist AS [Team]
 ,dim_matter_header_current.client_name
+,CASE WHEN dim_detail_claim.date_of_next_review	>GETDATE() THEN 1 ELSE 0 END AS CountofNextReview
 ,client_group_name
 ,dim_fed_hierarchy_history.name AS [Matter Owner]
 ,matter_description AS [Matter Description]
@@ -78,7 +80,8 @@ LEFT OUTER JOIN red_dw.dbo.dim_fed_hierarchy_history ON dim_fed_hierarchy_histor
 INNER JOIN #Division AS Division ON Division.ListValue  COLLATE DATABASE_DEFAULT = hierarchylevel2hist COLLATE DATABASE_DEFAULT
 INNER JOIN #Department AS Department ON Department.ListValue COLLATE DATABASE_DEFAULT = hierarchylevel3hist COLLATE DATABASE_DEFAULT
 INNER JOIN  #Team AS Team ON Team.ListValue COLLATE DATABASE_DEFAULT = REPLACE(hierarchylevel4hist,',','')	  COLLATE DATABASE_DEFAULT
---INNER JOIN #Status ON #Status.ListValue = CASE WHEN dim_matter_header_current.date_closed_case_management IS NULL THEN 'Open' ELSE 'Closed' END
+INNER JOIN #Status ON #Status.ListValue = CASE WHEN dim_matter_header_current.date_closed_case_management IS NULL THEN 'Open' ELSE 'Closed' END
+INNER JOIN #FeeEarner ON #FeeEarner.ListValue COLLATE DATABASE_DEFAULT = dim_matter_header_current.matter_owner_full_name
 LEFT OUTER JOIN red_dw.dbo.dim_detail_core_details ON  dim_detail_core_details.dim_detail_core_detail_key = fact_dimension_main.dim_detail_core_detail_key
 LEFT OUTER JOIN red_dw.dbo.dim_detail_outcome ON dim_detail_outcome.dim_detail_outcome_key = fact_dimension_main.dim_detail_outcome_key
 LEFT OUTER JOIN red_dw.dbo.dim_detail_claim ON red_dw.dbo.dim_detail_claim.dim_detail_claim_key = fact_dimension_main.dim_detail_claim_key
