a8d81534fd71e5d19ea8e086d9d77d73c7af2540
Jamie Bonner <Jamie.Bonner@weightmans.com>
2021-05-12 14:31:58 +0000

    #91950 change of columns needed by client
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16431

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.camerons_brewery_instructions.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.camerons_brewery_instructions.sql
@@ -9,6 +9,7 @@ GO
 -- Create date: 2021-03-16
 -- Description: #91950 New report request for Camerons Brewery. 
 				Shows number of instructions received, split by Current Status
+-- Update: #91950 change of columns needed by client
 -- =============================================
 */
 
@@ -23,34 +24,26 @@ BEGIN
 
 SELECT 
 	dim_matter_header_current.master_client_code + '/' + dim_matter_header_current.master_matter_number			AS [MatterSphere Client/Matter Number]
+	, dim_matter_header_current.matter_owner_full_name			AS [Case Handler]
 	, dim_matter_header_current.matter_description			AS [Matter Description]
-	, dim_detail_claim.camerons_brewery_specific_legal_charge		AS [Specific Legal Charge]
-	, dim_matter_header_current.matter_owner_full_name + ' & ' + dim_matter_header_current.matter_team_manager	AS [Weightmans Responsibility]
-	, dim_detail_claim.camerons_brewery_current_status			AS [Current Status]
-	, dim_detail_claim.camerons_brewery_next_weeks_steps		AS [Next Weeks Steps]
-	, CONVERT(DATE, dim_detail_claim.camerons_brewery_anticipated_completion_date, 103)		AS [Anticipated Completion Date]
-	, dim_detail_claim.camerons_brewery_last_updated			AS [Last Updated]
-	, CONVERT(DATE, dim_matter_header_current.date_opened_practice_management, 103)		AS [Date Opened]
+	, dim_file_notes.external_file_notes					AS [Present Position]
 	, CASE
-		WHEN dim_detail_claim.camerons_brewery_current_status IS NULL THEN
-			0
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'Current' THEN
-			1
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'Post Completion' THEN
-			2
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'On Hold' THEN
-			3
-	  END					AS sort_order
+		WHEN RTRIM(dim_detail_finance.output_wip_fee_arrangement) = 'Fixed Fee/Fee Quote/Capped Fee' THEN 
+			fact_finance_summary.fixed_fee_amount
+		ELSE
+			fact_finance_summary.revenue_estimate_net_of_vat
+	  END					AS [Agreed Fee]
+	, ISNULL(fact_finance_summary.defence_costs_billed, 0)			AS [Fees billed to Date]
+	, ISNULL(monthly_fees.Revenue, 0)			AS [Monthly Fees]
+	, ISNULL(fact_finance_summary.revenue_estimate_net_of_vat, 0) - ISNULL(fact_finance_summary.defence_costs_billed, 0)			AS [Fees to be Billed]
+	, CAST(dim_matter_header_current.date_opened_practice_management AS DATE)		AS [Date Opened]
+	, CAST(dim_matter_header_current.date_closed_practice_management AS DATE)		AS [Date Closed]
 	, CASE
-		WHEN dim_detail_claim.camerons_brewery_current_status IS NULL THEN
-			'Incomplete Current Status'
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'Current' THEN
-			'Current Matters Instructed'
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'Post Completion' THEN
-			'Post Completion Matters'
-		WHEN dim_detail_claim.camerons_brewery_current_status = 'On Hold' THEN
-			'On Hold Matters'
-	  END					AS page_name
+		WHEN dim_matter_header_current.date_closed_practice_management IS NULL THEN
+			'Open'
+		ELSE
+			'Closed'
+	  END				AS [OpenClosed]
 FROM red_dw.dbo.fact_dimension_main
 	INNER JOIN red_dw.dbo.dim_matter_header_current
 		ON dim_matter_header_current.dim_matter_header_curr_key = fact_dimension_main.dim_matter_header_curr_key
@@ -58,33 +51,37 @@ FROM red_dw.dbo.fact_dimension_main
 		ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_dimension_main.dim_fed_hierarchy_history_key
 	LEFT OUTER JOIN red_dw.dbo.dim_detail_claim
 		ON dim_detail_claim.dim_detail_claim_key = fact_dimension_main.dim_detail_claim_key
+	LEFT OUTER JOIN red_dw.dbo.dim_file_notes
+		ON dim_file_notes.dim_file_notes_key = fact_dimension_main.dim_file_notes_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_finance
+		ON dim_detail_finance.dim_detail_finance_key = fact_dimension_main.dim_detail_finance_key
+	LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
+		ON fact_finance_summary.master_fact_key = fact_dimension_main.master_fact_key
+	LEFT OUTER JOIN(
+					SELECT 
+						fact_bill_activity.client_code
+						, fact_bill_activity.matter_number
+						, dim_bill_date.bill_cal_month
+						, SUM(fact_bill_activity.bill_amount) Revenue
+					FROM red_dw.dbo.fact_bill_activity
+						INNER JOIN red_dw.dbo.dim_bill_date 
+							ON fact_bill_activity.dim_bill_date_key=dim_bill_date.dim_bill_date_key
+					WHERE 
+						RTRIM(fact_bill_activity.client_code) = 'W22555'
+						AND dim_bill_date.bill_cal_year = YEAR(GETDATE())
+						AND dim_bill_date.bill_cal_month_no = MONTH(GETDATE())
+					GROUP BY 
+						fact_bill_activity.client_code
+						, fact_bill_activity.matter_number
+						, dim_bill_date.bill_cal_month
+				)	AS monthly_fees
+		ON monthly_fees.client_code = dim_matter_header_current.client_code
+			AND monthly_fees.matter_number = dim_matter_header_current.matter_number
 WHERE 1 = 1
 	AND dim_matter_header_current.master_client_code = 'W22555'
 	AND dim_matter_header_current.reporting_exclusions = 0
-
-UNION
-
-/*
-Union is to create the blank "Potential Anticipated Matters" page that always needs to appear on the report as a blank sheet with the same column headers
-*/
-
-SELECT 
-	''		AS [MatterSphere Client/Matter Number]
-	, ''			AS [Matter Description]
-	, ''		AS [Specific Legal Charge]
-	, ''	AS [Weightmans Responsibility]
-	, ''			AS [Current Status]
-	, ''		AS [Next Weeks Steps]
-	, NULL		AS [Anticipated Completion Date]
-	, NULL			AS [Last Updated]
-	, NULL		AS [Date Opened]
-	, 99				AS sort_order --allows more sort_order categories to be added to above query
-	, 'Potential Anticipated Matters'				AS page_name
-
 ORDER BY
-	sort_order
-	, [Date Opened]
-
+	OpenClosed DESC
 
 END	
 GO
