820f83c9a9e7e0f381c43ad4b64a57409482bac1
6237 <6237@sbc.tfs.local>
2021-05-25 12:58:58 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16520

---
+++ MattersphereStaging - SVR-LIV-MSSQL-01/Tables/dbo.Request5870.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Request5870]
+(
+[File] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[Matter Number] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[Current] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
+[FileID] [float] NULL,
+[NewFileType] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
