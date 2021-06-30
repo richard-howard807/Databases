b96af37eb609850a3a56c73fed1c0b74d71f5ddf
6237 <6237@sbc.tfs.local>
2021-06-30 10:27:40 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C16734

---
+++ red_dw - SVR-LIV-DW-01/Functions/dbo.get_ms_employee_name_from_username.sql
@@ -0,0 +1,29 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE FUNCTION [dbo].[get_ms_employee_name_from_username]
+(
+@cdCode AS nvarchar(15)
+) RETURNS nvarchar(255)
+AS
+BEGIN
+
+  DECLARE
+  @lkuptxt    nvarchar(255) 
+
+  SET @lkuptxt = NULL
+
+  BEGIN
+
+  select @lkuptxt= ds_sh_ms_dbuser.usrfullname
+  from dbo.ds_sh_ms_dbuser
+  where ds_sh_ms_dbuser.usradid = @cdCode
+
+   END
+
+RETURN  @lkuptxt
+
+END
+GO
