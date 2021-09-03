0a1d133a22dbd7c005a2004273978de51b81aae6
Jamie Bonner <Jamie.Bonner@weightmans.com>
2021-09-03 09:37:11 +0000

    #112081 - Added CBO clients as per ticket rather than create a new report as they want the same data
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17010

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/marketing.client_generator_report_summary.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/marketing.client_generator_report_summary.sql
@@ -2,17 +2,25 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+--USE [Reporting]
+--GO
+--/****** Object:  StoredProcedure [marketing].[client_generator_report_summary]    Script Date: 01/09/2021 13:48:21 ******/
+--SET ANSI_NULLS ON
+--GO
+--SET QUOTED_IDENTIFIER ON
+--GO
 
 
 
 
--- =============================================
--- Author:		Lucy Dickinson
--- Create date: 20/11/2017
--- Description: Stored Procedure for the Generator Programm Report in the Marketing folder
---===============================================
--- 19/02/2019 LD  Added a new Generator Status called Pipeline and tried to improve the report a bit
---					Rewrote the current generator programme report to deal with Pipeline clients and just tweaked to improve experience
+---- =============================================
+---- Author:		Lucy Dickinson
+---- Create date: 20/11/2017
+---- Description: Stored Procedure for the Generator Programm Report in the Marketing folder
+----===============================================
+---- 19/02/2019 LD  Added a new Generator Status called Pipeline and tried to improve the report a bit
+----					Rewrote the current generator programme report to deal with Pipeline clients and just tweaked to improve experience
+---- 03/09/2021 JB	Added CBO clients for ticket #112091 rather than create a new report as they want the same data
 
 CREATE PROCEDURE [marketing].[client_generator_report_summary]
 (
@@ -24,7 +32,7 @@ AS
 	SET NOCOUNT ON;
 	
     --=== For testing purposes=============
-	--DECLARE @fin_period INT = 202112
+	--DECLARE @fin_period INT = 202204
 	--======================================
 
 
@@ -78,13 +86,63 @@ AS
 	SELECT @StartDate = MIN(dim_date_key) FROM red_dw.dbo.dim_date WHERE fin_year = @PrevFinancialYearMinus1 AND fin_month_no = 1
 	PRINT @StartDate
 	
-	
+
+--===========================================================================================================================================	
+	IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#cbo_clients')) 
+					DROP TABLE #cbo_clients
+
+
+SELECT DISTINCT
+	dim_client.client_code
+	, CASE
+		WHEN ISNULL(NULLIF(dim_client.client_group_name, ''), dim_client.client_name) = 'Global orphan group'	 THEN
+			'HealthNet Homecare (UK) Limited' 
+		ELSE
+			ISNULL(NULLIF(dim_client.client_group_name, ''), dim_client.client_name)
+	  END																			AS client_name
+	, NULL AS [cboGenClientStatus]
+	, 'cbo'		AS generator_category
+	, 'cbo'		AS report_type
+INTO #cbo_clients
+FROM red_dw.dbo.dim_client
+WHERE
+	ISNULL(NULLIF(dim_client.client_group_name, ''), dim_client.client_name) IN (
+	N'All About Food Limited',
+	N'OBG',
+	N'S Norton & Co Ltd',
+	N'Edge Worldwide Logistics Ltd',
+	N'WHT Holdings Limied',
+	N'Albany Products Limited',
+	N'Dennis Distribution',
+	N'Dennis Distribution LLP',
+	N'DENNIS DISTRIBUTION',
+	N'Mach Recruitment Limited',
+	N'Ultima Furniture Systems Limited',
+	N'PSI Global Ltd',
+	N'Gadcap Technical Solutions Limited',
+	N'HiComply Ltd',
+	N'Blackrose Management Limited',
+	N'Gaist',
+	N'Ansador Limited',
+	N'Empresaria Group plc',
+	N'Pure World Energy Limited',
+	N'Sontay Limited',
+	N'Turners Soham Turners Soham',
+	N'McGills Bus Company',
+	N'SB Drug Discovery Limited'
+	)
+	OR ISNULL(NULLIF(dim_client.client_group_name, ''), dim_client.client_name) LIKE 'Sandal Motors (Bayern) Limited%'
+	OR dim_client.client_code = 'W23164'
+
+
+--===========================================================================================================================================
 	IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#GeneratorStatus')) 
 					DROP TABLE #GeneratorStatus
 SELECT  
 			DISTINCT cboGenClientStatus AS[cboGenClientStatus]
 			,ISNULL(NULLIF(client_data.group_name,''), client_data.client_name)[client_group_name]
 			,client_code
+			, 'generator'		AS report_type
 INTO #GeneratorStatus
 FROM (
 	SELECT DISTINCT
@@ -202,6 +260,7 @@ WHERE 1 = 1
 				--END [generator_category]
 		,CASE WHEN dim_client.client_group_name IN ('BES Group','Bibby Group') THEN 'Star' ELSE 'Patron' END  AS [generator_category] -- Sarah Gerrad asked for them all to show as Parton
 		,[cboGenClientStatus] [category_code]
+		, generator.report_type	AS report_type
 		,SUM(CASE WHEN fin_year = @PrevFinancialYearMinus1 THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [fin_year_minus_2]
 		,SUM(CASE WHEN fin_year = @PreviousFinancialYear THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [fin_year_minus_1]
 		,SUM(CASE WHEN calendar_date BETWEEN @PrevFinYearStart AND @PrevPeriodEnd THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [prev_ytd]
@@ -220,7 +279,41 @@ WHERE 1 = 1
 		,dim_client.sector
 		,dim_client.segment
 		,[cboGenClientStatus]
+		, generator.report_type
 		,CASE WHEN dim_client.client_group_name IN ('BES Group','Bibby Group') THEN 'Star' ELSE 'Patron' END
+
+	UNION
+    
+	SELECT 
+			
+		#cbo_clients.client_name AS client_group_name
+		,dim_client.sector
+		,dim_client.segment
+		,#cbo_clients.generator_category
+		,[cboGenClientStatus] [category_code]
+		, #cbo_clients.report_type		AS report_type
+		,SUM(CASE WHEN fin_year = @PrevFinancialYearMinus1 THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [fin_year_minus_2]
+		,SUM(CASE WHEN fin_year = @PreviousFinancialYear THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [fin_year_minus_1]
+		,SUM(CASE WHEN calendar_date BETWEEN @PrevFinYearStart AND @PrevPeriodEnd THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [prev_ytd]
+		,SUM(CASE WHEN calendar_date BETWEEN @CurrFinYearStart AND @CurrPeriodEnd THEN [fact_bill_activity].[bill_amount] ELSE 0 END) [curr_ytd]
+
+	
+	FROM [red_dw].[dbo].[fact_bill_activity] fact_bill_activity
+	INNER JOIN red_dw.dbo.dim_client dim_client ON dim_client.dim_client_key = fact_bill_activity.dim_client_key
+	INNER JOIN red_dw.dbo.dim_date billed_date ON fact_bill_activity.dim_bill_date_key = billed_date.dim_date_key
+	INNER JOIN #cbo_clients ON #cbo_clients.client_code = fact_bill_activity.client_code 
+    WHERE  billed_date.calendar_date BETWEEN @PrevFinYearMinus1Start AND @CurrPeriodEnd
+	AND fact_bill_activity.client_code <> '00069873'
+	AND dim_client.client_code NOT IN ('00030645','95000C','00453737')
+	GROUP BY 
+  		#cbo_clients.client_name
+		,dim_client.sector
+		,dim_client.segment
+		, #cbo_clients.generator_category
+		, [cboGenClientStatus]
+		, #cbo_clients.report_type
+		
+
 	ORDER BY category_code
 	
 	
