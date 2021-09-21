bf09f8ee11e853a4c8e1e63cd6312d46d6bcd7c2
6237 <6237@sbc.tfs.local>
2021-09-21 13:29:47 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17066

--- Visualisation - SVR-LIV-DW-01/Stored Procedures/dbo.ClientMINewClientOpening.sql
+++ Visualisation - SVR-LIV-DW-01/Stored Procedures/dbo.ClientMINewClientOpening.sql
@@ -4,6 +4,8 @@ SET ANSI_NULLS ON
 GO
 
 
+
+
 CREATE PROCEDURE [dbo].[ClientMINewClientOpening] 
 
 AS
@@ -16,12 +18,19 @@ DECLARE @Month AS INT
 SET @Year=(SELECT DISTINCT fin_year FROM red_dw.dbo.dim_date WHERE CONVERT(DATE,calendar_date,103)=CONVERT(DATE,DATEADD(m,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE()), 0)),103))
 SET @Month=(SELECT DISTINCT fin_month_no FROM red_dw.dbo.dim_date WHERE CONVERT(DATE,calendar_date,103)=CONVERT(DATE,DATEADD(m,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE()), 0)),103))
 
-PRINT @Month
+DECLARE @StartDate AS DATE
+DECLARE @EndDate AS DATE 
+
+SET @StartDate=(SELECT MIN(calendar_date) FROM red_dw.dbo.dim_date WHERE fin_year=@Year AND fin_month_no<=@Month)
+SET @EndDate=(SELECT MAX(calendar_date) FROM red_dw.dbo.dim_date WHERE fin_year=@Year AND fin_month_no<=@Month)
+
+
+
 
 SELECT dim_client.segment 
 ,DATEADD(m,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE()), 0)) AS [Month]
-,NULL AS [New/Linked/Re-opened Previous YTD]
-,NULL AS [New/Linked/Re-opened YTD]
+,SUM(NewClientsCreatedPrevious) + SUM(ReopenPrev.ReopeningsPrev) AS [New/Linked/Re-opened Previous YTD]
+,SUM(NewCreatedCurrent.NewClientsCreated) + SUM(ReopenCurrent.ReopeningsCurrent) AS [New/Linked/Re-opened YTD]
 ,SUM(PreviousYear.NewClientYTD) AS [New Clients Billed Previous YTD]
 ,SUM(CurrentYear.NewClientYTD)  AS [New Clients Billed YTD]
 ,SUM(PreviousYear.RevenueYTD) AS [New Clients Billing Previous YTD]
@@ -73,6 +82,173 @@ AND bill_fin_month_no<=@Month
 AND sector IS NOT NULL
 
 GROUP BY segment) AS PreviousYear ON PreviousYear.segment = dim_client.segment
+
+LEFT OUTER JOIN
+(
+ SELECT segment,
+ COUNT(1) AS NewClientsCreated 
+
+FROM MS_Prod.config.dbClient 
+INNER JOIN red_dw.dbo.dim_client
+ ON clNo=client_code COLLATE DATABASE_DEFAULT
+INNER JOIN MS_Prod.dbo.udExtClient udExtClient  ON udExtClient.clID = dbClient.clID
+INNER JOIN red_dw.dbo.dim_date
+ ON CONVERT(DATE,calendar_date,103)=CONVERT(DATE,dbClient.Created,103)
+WHERE 1=1
+AND fin_year=@Year AND fin_month_no<=@Month
+AND dbClient.clName NOT LIKE '%MS TEST%'
+AND UPPER(dbClient.clName) NOT LIKE '%TEST%'
+AND UPPER(dbClient.clName) NOT LIKE '%ERROR%'
+AND dbClient.clNo NOT LIKE 'EMP%'
+GROUP BY segment
+) AS NewCreatedCurrent
+ ON NewCreatedCurrent.segment = dim_client.segment
+LEFT OUTER JOIN
+(
+ SELECT segment,
+ COUNT(1) AS NewClientsCreatedPrevious 
+
+FROM MS_Prod.config.dbClient 
+INNER JOIN red_dw.dbo.dim_client
+ ON clNo=client_code COLLATE DATABASE_DEFAULT
+INNER JOIN MS_Prod.dbo.udExtClient udExtClient  ON udExtClient.clID = dbClient.clID
+INNER JOIN red_dw.dbo.dim_date
+ ON CONVERT(DATE,calendar_date,103)=CONVERT(DATE,dbClient.Created,103)
+WHERE 1=1
+AND fin_year=@Year-1 AND fin_month_no<=@Month
+AND dbClient.clName NOT LIKE '%MS TEST%'
+AND UPPER(dbClient.clName) NOT LIKE '%TEST%'
+AND UPPER(dbClient.clName) NOT LIKE '%ERROR%'
+AND dbClient.clNo NOT LIKE 'EMP%'
+GROUP BY segment
+) AS NewCreatedPrevious
+ ON NewCreatedPrevious.segment = dim_client.segment
+
+LEFT OUTER JOIN 
+(
+SELECT segment,COUNT(1) AS ReopeningsCurrent
+FROM 
+(
+SELECT	MostRecent.mg_client	AS Client
+	--,	caclient.cl_clname		AS ClientName
+	,	MostRecent.mg_matter	AS Matter
+	,	MostRecent.mg_feearn	AS FeeEarner
+	,	MostRecent.mg_datopn	AS DateMatterOpened
+	,	Previous.mg_feearn		AS	PreviousFeeEarner
+	,	Previous.mg_datopn		AS	PreviousDateMatterOpened
+	,	DATEDIFF(YEAR,Previous.mg_datopn,MostRecent.mg_datopn) AS NoYears
+FROM
+	(
+	SELECT	client_code AS mg_client
+			,matter_number AS mg_matter
+			,matter_owner_full_name AS 	mg_feearn
+			,date_opened_practice_management AS 	mg_datopn
+			,ROW_NUMBER() OVER (PARTITION BY client_code ORDER BY date_opened_practice_management DESC ) AS OrderID 
+			FROM red_dw.dbo.dim_matter_header_current
+			WHERE matter_number <>'ML'
+	)	MostRecent
+
+INNER JOIN 
+	(
+			SELECT	client_code AS mg_client
+			,matter_number AS mg_matter
+			,matter_owner_full_name AS 	mg_feearn
+			,date_opened_practice_management AS 	mg_datopn
+			,ROW_NUMBER() OVER (PARTITION BY client_code ORDER BY date_opened_practice_management DESC ) AS OrderID 
+			FROM red_dw.dbo.dim_matter_header_current
+			WHERE matter_number <>'ML'
+	)	Previous
+	
+	ON	MostRecent.mg_client = Previous.mg_client
+INNER JOIN red_dw.dbo.dim_date
+ ON CONVERT(DATE,MostRecent.mg_datopn,103)=CONVERT(DATE,calendar_date,103)
+
+WHERE	MostRecent.OrderID = 1
+	AND Previous.OrderID = 2
+	AND	DATEDIFF(YEAR,Previous.mg_datopn,MostRecent.mg_datopn) >= 3
+	AND fin_year=@Year AND fin_month_no<=@Month
+
+) AS AllData
+LEFT OUTER JOIN red_dw.dbo.dim_client
+ ON AllData.Client=client_code
+LEFT OUTER JOIN (
+SELECT dim_client.client_code,COUNT(1) AS TotalMatters ,SUM(defence_costs_billed) AS Revenue
+FROM red_dw.dbo.dim_client
+INNER JOIN red_dw.dbo.dim_matter_header_current
+ ON dim_matter_header_current.client_code = dim_client.client_code
+INNER JOIN red_dw.dbo.fact_finance_summary
+ ON fact_finance_summary.client_code = dim_matter_header_current.client_code
+ AND fact_finance_summary.matter_number = dim_matter_header_current.matter_number
+GROUP BY dim_client.client_code) AS TotalMatters
+ ON TotalMatters.client_code = dim_client.client_code
+WHERE DATEDIFF(MONTH,PreviousDateMatterOpened,DateMatterOpened)/12>=3
+GROUP BY segment
+) AS ReopenCurrent
+ ON ReopenCurrent.segment = dim_client.segment
+LEFT OUTER JOIN 
+(
+SELECT segment,COUNT(1) AS ReopeningsPrev
+FROM 
+(
+SELECT	MostRecent.mg_client	AS Client
+	--,	caclient.cl_clname		AS ClientName
+	,	MostRecent.mg_matter	AS Matter
+	,	MostRecent.mg_feearn	AS FeeEarner
+	,	MostRecent.mg_datopn	AS DateMatterOpened
+	,	Previous.mg_feearn		AS	PreviousFeeEarner
+	,	Previous.mg_datopn		AS	PreviousDateMatterOpened
+	,	DATEDIFF(YEAR,Previous.mg_datopn,MostRecent.mg_datopn) AS NoYears
+FROM
+	(
+	SELECT	client_code AS mg_client
+			,matter_number AS mg_matter
+			,matter_owner_full_name AS 	mg_feearn
+			,date_opened_practice_management AS 	mg_datopn
+			,ROW_NUMBER() OVER (PARTITION BY client_code ORDER BY date_opened_practice_management DESC ) AS OrderID 
+			FROM red_dw.dbo.dim_matter_header_current
+			WHERE matter_number <>'ML'
+	)	MostRecent
+
+INNER JOIN 
+	(
+			SELECT	client_code AS mg_client
+			,matter_number AS mg_matter
+			,matter_owner_full_name AS 	mg_feearn
+			,date_opened_practice_management AS 	mg_datopn
+			,ROW_NUMBER() OVER (PARTITION BY client_code ORDER BY date_opened_practice_management DESC ) AS OrderID 
+			FROM red_dw.dbo.dim_matter_header_current
+			WHERE matter_number <>'ML'
+	)	Previous
+	
+	ON	MostRecent.mg_client = Previous.mg_client
+INNER JOIN red_dw.dbo.dim_date
+ ON CONVERT(DATE,MostRecent.mg_datopn,103)=CONVERT(DATE,calendar_date,103)
+
+WHERE	MostRecent.OrderID = 1
+	AND Previous.OrderID = 2
+	AND	DATEDIFF(YEAR,Previous.mg_datopn,MostRecent.mg_datopn) >= 3
+	AND fin_year=@Year-1 AND fin_month_no<=@Month
+
+) AS AllData
+LEFT OUTER JOIN red_dw.dbo.dim_client
+ ON AllData.Client=client_code
+LEFT OUTER JOIN (
+SELECT dim_client.client_code,COUNT(1) AS TotalMatters ,SUM(defence_costs_billed) AS Revenue
+FROM red_dw.dbo.dim_client
+INNER JOIN red_dw.dbo.dim_matter_header_current
+ ON dim_matter_header_current.client_code = dim_client.client_code
+INNER JOIN red_dw.dbo.fact_finance_summary
+ ON fact_finance_summary.client_code = dim_matter_header_current.client_code
+ AND fact_finance_summary.matter_number = dim_matter_header_current.matter_number
+GROUP BY dim_client.client_code) AS TotalMatters
+ ON TotalMatters.client_code = dim_client.client_code
+WHERE DATEDIFF(MONTH,PreviousDateMatterOpened,DateMatterOpened)/12>=3
+GROUP BY segment
+) AS ReopenPrev
+ ON ReopenPrev.segment = dim_client.segment
+  
+
+
 WHERE dim_client.segment IS NOT NULL
 GROUP BY dim_client.segment 
 
