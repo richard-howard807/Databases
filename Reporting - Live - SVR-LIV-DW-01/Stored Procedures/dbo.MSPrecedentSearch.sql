4eecd6283028e96d9dd0a63f78fd0aa736cf1c2d
6237 <6237@sbc.tfs.local>
2021-11-03 11:14:25 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17271

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.MSPrecedentSearch.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.MSPrecedentSearch.sql
@@ -5,13 +5,20 @@ GO
 
 
 
+
+
 CREATE PROCEDURE [dbo].[MSPrecedentSearch]
 (
 @StartDate AS DATE
 ,@EndDate AS DATE
 ,@SearchString AS NVARCHAR(MAX)
+,@Client AS NVARCHAR(100)
 )
 AS 
+
+BEGIN
+
+IF @Client IS NULL  
 
 BEGIN
 
@@ -46,6 +53,81 @@ SELECT
 		
 		)
 		ORDER BY dbDocument.Created
+END 
+
+
+
+ELSE 
+
+IF @Client IS NOT NULL AND @SearchString IS NULL 
+
+BEGIN 
+SELECT 
+       dbFile.fileID,
+          dbClient.clNo [Client],
+       dbFile.fileNo [Matter],
+       dbPrec.PrecDesc,
+       dbPrec.PrecPubName,
+	   dbPrec.PrecTitle,
+	   docID,
+	   docDesc,
+       MS_PROD.config.dbDocument.Created [Date Document Created],
+       dbUser.usrFullName [Created by Name],
+       FileOwner.usrFullName [Matter Owner]
+		  FROM ms_prod.config.dbDocument WITH(NOLOCK)
+  INNER JOIN MS_Prod.dbo.dbPrecedents dbPrec WITH(NOLOCK)
+        ON dbDocument.docbaseprecid = dbPrec.PrecID
+	  LEFT JOIN MS_Prod.config.dbFile dbFile
+        ON dbFile.fileID = dbDocument.fileID
+    LEFT JOIN MS_Prod.config.dbClient dbClient
+        ON dbFile.clID = dbClient.clID
+  LEFT JOIN MS_Prod.dbo.dbUser dbUser
+        ON dbUser.usrID = dbdocument.Createdby
+    LEFT JOIN MS_Prod.dbo.dbUser FileOwner
+        ON FileOwner.usrID = dbFile.filePrincipleID
+		WHERE dbDocument.Created BETWEEN @StartDate AND @EndDate
+		AND dbClient.clNo=@Client
+		ORDER BY dbDocument.Created
+END 
+
+ELSE 
+
+BEGIN
+
+SELECT 
+       dbFile.fileID,
+          dbClient.clNo [Client],
+       dbFile.fileNo [Matter],
+       dbPrec.PrecDesc,
+       dbPrec.PrecPubName,
+	   dbPrec.PrecTitle,
+	   docID,
+	   docDesc,
+       MS_PROD.config.dbDocument.Created [Date Document Created],
+       dbUser.usrFullName [Created by Name],
+       FileOwner.usrFullName [Matter Owner]
+		  FROM ms_prod.config.dbDocument WITH(NOLOCK)
+  INNER JOIN MS_Prod.dbo.dbPrecedents dbPrec WITH(NOLOCK)
+        ON dbDocument.docbaseprecid = dbPrec.PrecID
+	  LEFT JOIN MS_Prod.config.dbFile dbFile
+        ON dbFile.fileID = dbDocument.fileID
+    LEFT JOIN MS_Prod.config.dbClient dbClient
+        ON dbFile.clID = dbClient.clID
+  LEFT JOIN MS_Prod.dbo.dbUser dbUser
+        ON dbUser.usrID = dbdocument.Createdby
+    LEFT JOIN MS_Prod.dbo.dbUser FileOwner
+        ON FileOwner.usrID = dbFile.filePrincipleID
+		WHERE dbDocument.Created BETWEEN @StartDate AND @EndDate
+		
+		AND (
+		LOWER(RTRIM(dbPrec.PrecDesc)) LIKE '%'+LOWER(@SearchString)+'%' OR
+		LOWER(RTRIM(dbPrec.PrecTitle)) LIKE '%'+LOWER(@SearchString)+'%'
+		
+		)
+		AND dbClient.clNo=@Client
+		ORDER BY dbDocument.Created
+
+		END 
 
 		END 
 GO
