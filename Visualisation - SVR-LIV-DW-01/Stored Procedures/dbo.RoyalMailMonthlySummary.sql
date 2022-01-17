923b56a1cb87678348c9d9b2799303a749532d0d
6237 <6237@sbc.tfs.local>
2022-01-17 09:49:25 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17557

--- Visualisation - SVR-LIV-DW-01/Stored Procedures/dbo.RoyalMailMonthlySummary.sql
+++ Visualisation - SVR-LIV-DW-01/Stored Procedures/dbo.RoyalMailMonthlySummary.sql
@@ -12,6 +12,7 @@ Current Version:	Initial Create
 ====================================================
 -- JB - 2020-09-08 - changed to look 2 months back to July for ticket #70140. Will revert to 1 month back once ticket closed off
 -- JB - 2020-09-08 - reverted back to original query ready for next months dashboard update
+-- JB - 2022-01-14 - added @fin_period/@previous_month/@current_year variables so proc doesn't need changing in January to deal with December
 ====================================================
 
 */
@@ -20,6 +21,25 @@ AS
 BEGIN
 
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+DECLARE @fin_period AS VARCHAR(20) = (SELECT TOP 1
+										last_month.last_month
+									FROM (
+										SELECT TOP 1
+											dim_date.fin_period
+											, LAG(dim_date.fin_period, 1) OVER	(ORDER BY dim_date.fin_period) AS last_month
+										FROM red_dw.dbo.dim_date
+										WHERE
+											dim_date.calendar_date <= CAST(GETDATE() AS DATE)
+										ORDER BY
+											dim_date.fin_period DESC
+										) AS last_month
+									)
+DECLARE @previous_month AS INT = (SELECT DISTINCT dim_date.cal_month_no FROM red_dw.dbo.dim_date WHERE dim_date.fin_period = @fin_period)
+DECLARE @current_year AS INT = (SELECT DISTINCT dim_date.cal_year FROM red_dw.dbo.dim_date WHERE dim_date.fin_period = @fin_period)
+DECLARE @current_month AS INT = (SELECT IIF(MONTH(GETDATE()) = 1, 0, MONTH(GETDATE()))) 
+
+--SELECT @fin_period, @previous_month, @current_year, @current_month 
 
 --uncomment for january and comment out below select as it deals with december
 
@@ -43,13 +63,20 @@ SELECT dim_matter_header_current.client_code AS [Client]
 			, NULL [Bill Date (year)]
 	
 	
-			, DATEPART(MONTH, GETDATE())-1 [Previous Month]
-			, DATEPART(YEAR, GETDATE()) [Current Year]
-			, CASE WHEN (DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) = DATEPART(MONTH, GETDATE())-1  AND DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [New (Month)]
+		--	, DATEPART(MONTH, GETDATE())-1 [Previous Month]
+		--	, DATEPART(YEAR, GETDATE()) [Current Year]
+		--	, CASE WHEN (DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) = DATEPART(MONTH, GETDATE())-1  AND DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [New (Month)]
 
-		, CASE WHEN (DATEPART(MONTH, dim_detail_property.[completion_date]) = DATEPART(MONTH, GETDATE())-1 AND DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [Completed (Month)]
-		, CASE WHEN DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE()) AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [New (Year)]
-		, CASE WHEN DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE()) AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [Completed (Year)]
+		--, CASE WHEN (DATEPART(MONTH, dim_detail_property.[completion_date]) = DATEPART(MONTH, GETDATE())-1 AND DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [Completed (Month)]
+		--, CASE WHEN DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE()) AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [New (Year)]
+		--, CASE WHEN DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE()) AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [Completed (Year)]
+		, @previous_month [Previous Month]
+		, @current_year [Current Year]
+		, CASE WHEN dim_date_opened.fin_period = @fin_period THEN '1' ELSE '0' END [New (Month)]
+
+		, CASE WHEN dim_date_completed.fin_period = @fin_period THEN '1' ELSE '0' END [Completed (Month)]
+		, CASE WHEN dim_date_opened.cal_year = @current_year AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> @current_month THEN '1' ELSE '0' END [New (Year)]
+		, CASE WHEN dim_date_completed.cal_year = @current_year AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> @current_month THEN '1' ELSE '0' END [Completed (Year)]
 		, 'Matter Level' [Level]
 
 
@@ -60,8 +87,10 @@ LEFT OUTER JOIN red_dw.dbo.dim_detail_property ON dim_detail_property.dim_detail
 LEFT OUTER JOIN red_dw.dbo.dim_fed_hierarchy_history ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_dimension_main.dim_fed_hierarchy_history_key
 LEFT OUTER JOIN red_dw.dbo.dim_department ON dim_department.dim_department_key = dim_matter_header_current.dim_department_key
 LEFT OUTER JOIN red_dw..dim_instruction_type ON dim_instruction_type.dim_instruction_type_key = dim_matter_header_current.dim_instruction_type_key
-
-
+INNER JOIN red_dw.dbo.dim_date AS dim_date_opened
+	ON dim_date_opened.calendar_date = CAST(dim_matter_header_current.date_opened_case_management AS DATE)
+LEFT OUTER JOIN red_dw.dbo.dim_date AS dim_date_completed
+	ON dim_date_completed.calendar_date = CAST(dim_detail_property.completion_date AS DATE)
 WHERE (dim_matter_header_current.client_code IN ('P00010', 'P00011', 'P00012', 'P00020', 'P00021', 'P00022') 
 OR (dim_detail_property.client_code='R1001' AND dim_instruction_type.instruction_type IN ('Real Estate Transactional Work (Volume)','Real Estate Litigation Work (Volume)', 'Real Estate Advisory Work (Volume)','Real Estate Transactional Work (BAU)','Real Estate Litigation Work (BAU)','Real Estate Advisory Work (BAU)')))
 AND dim_matter_header_current.matter_number <> 'ML'
@@ -85,18 +114,26 @@ SELECT dim_matter_header_current.client_code AS [Client Code]
 		, [dbo].[ReturnElapsedDaysExcludingBankHolidays] (date_opened_practice_management,coalesce( dim_detail_property.[completion_date],dim_detail_property.date_elements_agreed)) AS [Days to Complete Cases]
 		, CASE WHEN dim_detail_property.[completion_date] IS NULL THEN '0' ELSE '1' END AS [Completed Cases]
 		, [Legal Spend] AS [Legal Spend]
-		, [Current Legal Spend] AS [Current Legal Spend]
+		, [Current Legal Spend] AS [Current Month Legal Spend]
 		, dim_matter_header_current.date_closed_case_management AS [Date Closed]
 		, [InvDate] AS [Bill Date]
 		, DATEPART(MONTH,[InvDate]) AS [Bill Date (month)]
 		, DATEPART(YEAR,[InvDate]) AS [Bill Date (year)]
-		, DATEPART(MONTH, GETDATE())-1 [Previous Month]
-		, DATEPART(YEAR, GETDATE()) [Current Year]
-		, CASE WHEN (DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) = DATEPART(MONTH, GETDATE())-1  AND DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [New (Month)]
+		--, DATEPART(MONTH, GETDATE())-1 [Previous Month]
+		--, DATEPART(YEAR, GETDATE()) [Current Year]
+		--, CASE WHEN (DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) = DATEPART(MONTH, GETDATE())-1  AND DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [New (Month)]
 
-		, CASE WHEN (DATEPART(MONTH, dim_detail_property.[completion_date]) = DATEPART(MONTH, GETDATE())-1 AND DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [Completed (Month)]
-		, CASE WHEN DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE()) AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [New (Year)]
-		, CASE WHEN DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE()) AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [Completed (Year)]
+		--, CASE WHEN (DATEPART(MONTH, dim_detail_property.[completion_date]) = DATEPART(MONTH, GETDATE())-1 AND DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE())) THEN '1' ELSE '0' END [Completed (Month)]
+		--, CASE WHEN DATEPART(YEAR, dim_matter_header_current.date_opened_case_management) = DATEPART(YEAR, GETDATE()) AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [New (Year)]
+		--, CASE WHEN DATEPART(YEAR, dim_detail_property.[completion_date]) = DATEPART(YEAR, GETDATE()) AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> DATEPART(MONTH, GETDATE()) THEN '1' ELSE '0' END [Completed (Year)]
+		, @previous_month [Previous Month]
+		, @current_year [Current Year]
+		, CASE WHEN dim_date_opened.fin_period = @fin_period THEN '1' ELSE '0' END [New (Month)]
+
+		, CASE WHEN dim_date_completed.fin_period = @fin_period THEN '1' ELSE '0' END [Completed (Month)]
+		, CASE WHEN dim_date_opened.cal_year = @current_year AND DATEPART(MONTH, dim_matter_header_current.date_opened_case_management) <> @current_month THEN '1' ELSE '0' END  [New (Year)]
+		, CASE WHEN dim_date_completed.cal_year = @current_year AND  DATEPART(MONTH, dim_detail_property.[completion_date]) <> @current_month  THEN '1' ELSE '0' END [Completed (Year)]
+		
 		, 'Bill Level' AS [Level]
 
 
@@ -107,14 +144,18 @@ LEFT OUTER JOIN red_dw.dbo.dim_detail_property ON dim_detail_property.dim_detail
 LEFT OUTER JOIN red_dw.dbo.dim_fed_hierarchy_history ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_dimension_main.dim_fed_hierarchy_history_key
 LEFT OUTER JOIN red_dw.dbo.dim_department ON dim_department.dim_department_key = dim_matter_header_current.dim_department_key
 LEFT OUTER JOIN red_dw..dim_instruction_type ON dim_instruction_type.dim_instruction_type_key = dim_matter_header_current.dim_instruction_type_key
-
+INNER JOIN red_dw.dbo.dim_date AS dim_date_opened
+	ON dim_date_opened.calendar_date = CAST(dim_matter_header_current.date_opened_case_management AS DATE)
+LEFT OUTER JOIN red_dw.dbo.dim_date AS dim_date_completed
+	ON dim_date_completed.calendar_date = CAST(dim_detail_property.completion_date AS DATE)
 LEFT OUTER JOIN (SELECT Matters.Client
 						, Matters.Matter
 						, ARMaster.ARFee AS [ARFee]
 						, ARMaster.InvDate AS [InvDate]
-						, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = DATEPART(YEAR, GETDATE()) AND (DATEPART(MONTH, ARMaster.InvDate) <> DATEPART(MONTH, GETDATE()) AND DATEPART(MONTH, ARMaster.InvDate) <> DATEPART(MONTH, GETDATE())-1) THEN ARMaster.ARFee ELSE '0' END [Legal Spend]
-						, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = DATEPART(YEAR, GETDATE()) AND (DATEPART(MONTH, ARMaster.InvDate) = DATEPART(MONTH, GETDATE())-1) THEN ARMaster.ARFee ELSE '0' END [Current Legal Spend]
-
+						--, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = DATEPART(YEAR, GETDATE()) AND (DATEPART(MONTH, ARMaster.InvDate) <> DATEPART(MONTH, GETDATE()) AND DATEPART(MONTH, ARMaster.InvDate) <> DATEPART(MONTH, GETDATE())-1) THEN ARMaster.ARFee ELSE '0' END [Legal Spend]
+						--, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = DATEPART(YEAR, GETDATE()) AND (DATEPART(MONTH, ARMaster.InvDate) = DATEPART(MONTH, GETDATE())-1) THEN ARMaster.ARFee ELSE '0' END [Current Legal Spend]
+						, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = @current_year AND (DATEPART(MONTH, ARMaster.InvDate)) <> @current_month AND DATEPART(MONTH, ARMaster.InvDate) <> @previous_month THEN ARMaster.ARFee ELSE '0' END [Legal Spend]
+						, CASE WHEN DATEPART(YEAR, ARMaster.InvDate) = @current_year AND (DATEPART(MONTH, ARMaster.InvDate)) = @previous_month THEN ARMaster.ARFee ELSE '0' END [Current Legal Spend]
 				FROM  TE_3E_Prod.dbo.InvMaster WITH (NOLOCK) 
 				INNER JOIN  TE_3E_Prod.dbo.ARMaster WITH (NOLOCK)
 				ON InvMaster.InvIndex=ARMaster.InvMaster 
