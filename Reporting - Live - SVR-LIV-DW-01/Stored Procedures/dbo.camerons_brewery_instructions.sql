2e3d93ae0759f245bdd9a7ac2cdc85c515f4ed0a
Jamie Bonner <Jamie.Bonner@weightmans.com>
2022-03-03 10:21:53 +0000

    #136881 - changed revenue/disb figures to look at 3E due to WB matters revenue not being in warehouse pre merger
    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17765

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.camerons_brewery_instructions.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.camerons_brewery_instructions.sql
@@ -11,6 +11,7 @@ GO
 -- Description: #91950 New report request for Camerons Brewery. 
 				Shows number of instructions received, split by Current Status
 -- Update: #91950 change of columns needed by client
+-- JB ticket #136881 - changed revenue/disb figures to look at 3E due to WB matters revenue not being in warehouse pre merger 
 -- =============================================
 */
 
@@ -30,6 +31,43 @@ BEGIN
 --DECLARE @start_date	AS DATE = CONVERT(DATE,DATEADD(mm, DATEDIFF(mm, 0, GETDATE()) - 1, 0),103)
 --		, @end_date AS DATE = CONVERT(DATE,DATEADD(DAY, -(DAY(GETDATE())), GETDATE()),103) 
 
+DROP TABLE IF EXISTS #cb_bills
+
+SELECT 
+	bill_data.dim_matter_header_curr_key
+	, SUM(bill_data.revenue)	AS revenue
+	, SUM(bill_data.disbs)		AS disbs
+	, SUM(bill_data.monthly_fees) AS monthly_fees
+	, SUM(bill_data.monthly_disbs)		AS monthly_disbs
+INTO #cb_bills
+FROM (
+		SELECT 
+			dim_matter_header_current.dim_matter_header_curr_key
+			, ARDetail.InvDate	
+			, SUM(ARFee)		AS revenue
+			, SUM(ARDetail.ARHCo) + SUM(ARDetail.ARSCo)		AS disbs
+			, IIF(ARDetail.InvDate BETWEEN @start_date AND @end_date, SUM(ARDetail.ARFee), 0) AS monthly_fees
+			, IIF(ARDetail.InvDate BETWEEN @start_date AND @end_date, SUM(ARDetail.ARHCo) + SUM(ARDetail.ARSCo), 0) AS monthly_disbs
+		FROM TE_3E_Prod.dbo.ARDetail
+			INNER JOIN TE_3E_Prod.dbo.InvMaster
+				ON InvIndex=InvMaster
+			INNER JOIN TE_3E_Prod.dbo.Matter
+				ON InvMaster.LeadMatter = Matter.MattIndex
+			INNER JOIN MS_Prod.config.dbFile
+				ON Matter.MattIndex = dbFile.fileExtLinkID
+			INNER JOIN red_dw.dbo.dim_matter_header_current	
+				ON dim_matter_header_current.ms_fileid = dbFile.fileID
+		WHERE 1 = 1 
+			AND dim_matter_header_current.master_client_code IN ('W22555', 'W24107')
+			AND ARList IN ('Bill','BillRev')
+		GROUP BY
+			dim_matter_header_current.dim_matter_header_curr_key
+			, ARDetail.InvDate
+	) AS bill_data
+GROUP BY
+	bill_data.dim_matter_header_curr_key
+
+
 
 SELECT 
 	dim_matter_header_current.master_client_code + '/' + dim_matter_header_current.master_matter_number			AS [MatterSphere Client/Matter Number]
@@ -42,15 +80,15 @@ SELECT
 		ELSE
 			fact_finance_summary.revenue_estimate_net_of_vat
 	  END					AS [Agreed Fee]
-	, ISNULL(fact_finance_summary.defence_costs_billed, 0)			AS [Fees billed to Date]
-	, ISNULL(fact_finance_summary.disbursements_billed, 0)			AS [Disbursements Billed to Date]
-	, ISNULL(monthly_fees.Revenue, 0)			AS [Monthly Fees]
-	, ISNULL(DisbsBilled,0) AS [Monthly Disbs]
+	, ISNULL(#cb_bills.revenue, 0)			AS [Fees billed to Date]
+	, ISNULL(#cb_bills.disbs, 0)			AS [Disbursements Billed to Date]
+	, ISNULL(#cb_bills.monthly_fees, 0)			AS [Monthly Fees]
+	, ISNULL(#cb_bills.monthly_disbs,0) AS [Monthly Disbs]
 	, CASE
 		WHEN RTRIM(dim_detail_finance.output_wip_fee_arrangement) = 'Fixed Fee/Fee Quote/Capped Fee' THEN 
-			ISNULL(fact_finance_summary.fixed_fee_amount, 0) - ISNULL(fact_finance_summary.defence_costs_billed, 0)
+			ISNULL(fact_finance_summary.fixed_fee_amount, 0) - ISNULL(#cb_bills.revenue, 0)
 		ELSE
-			ISNULL(fact_finance_summary.revenue_estimate_net_of_vat, 0) - ISNULL(fact_finance_summary.defence_costs_billed, 0)
+			ISNULL(fact_finance_summary.revenue_estimate_net_of_vat, 0) - ISNULL(#cb_bills.revenue, 0)
 	  END													AS [Fees to be Billed]
 	, ISNULL(fact_finance_summary.disbursement_balance, 0)		AS [Unbilled Disbursements]
 	, CAST(dim_matter_header_current.date_opened_practice_management AS DATE)		AS [Date Opened]
@@ -75,35 +113,8 @@ FROM red_dw.dbo.fact_dimension_main
 		ON dim_detail_finance.dim_detail_finance_key = fact_dimension_main.dim_detail_finance_key
 	LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
 		ON fact_finance_summary.master_fact_key = fact_dimension_main.master_fact_key
-	LEFT OUTER JOIN(
-					SELECT 
-						fact_bill_activity.client_code
-						, fact_bill_activity.matter_number
-						, dim_bill_date.bill_cal_month
-						, SUM(fact_bill_activity.bill_amount) Revenue
-					FROM red_dw.dbo.fact_bill_activity
-						INNER JOIN red_dw.dbo.dim_bill_date 
-							ON fact_bill_activity.dim_bill_date_key=dim_bill_date.dim_bill_date_key
-					WHERE 
-						RTRIM(fact_bill_activity.client_code) IN ('W22555', 'W24107')
-						AND dim_bill_date.bill_date BETWEEN @start_date AND @end_date
-					GROUP BY 
-						fact_bill_activity.client_code
-						, fact_bill_activity.matter_number
-						, dim_bill_date.bill_cal_month
-				)	AS monthly_fees
-		ON monthly_fees.client_code = dim_matter_header_current.client_code
-			AND monthly_fees.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN (SELECT fact_bill.dim_matter_header_curr_key,SUM(paid_disbursements) + SUM(unpaid_disbursements) AS DisbsBilled
-FROM red_dw.dbo.fact_bill 
-INNER JOIN red_dw.dbo.dim_matter_header_current
- ON dim_matter_header_current.dim_matter_header_curr_key = fact_bill.dim_matter_header_curr_key
-INNER JOIN red_dw.dbo.dim_bill ON dim_bill.dim_bill_key = fact_bill.dim_bill_key
-INNER JOIN red_dw.dbo.dim_bill_date ON dim_bill_date.dim_bill_date_key = fact_bill.dim_bill_date_key 
-WHERE bill_date BETWEEN @start_date AND @end_date
-AND bill_reversed=0
-AND master_client_code IN ('W22555', 'W24107') GROUP BY fact_bill.dim_matter_header_curr_key) AS DisbursementsMonth
- ON DisbursementsMonth.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN #cb_bills
+		ON #cb_bills.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
 WHERE 1 = 1
 	AND dim_matter_header_current.master_client_code IN ('W22555', 'W24107')
 	--AND dim_matter_header_current.master_matter_number = '211'
