ba286746ded32f067856cb5db9bcc883d30a597f
6237 <6237@sbc.tfs.local>
2022-03-28 07:10:36 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C17881

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.Magenta_MagentaInjunctionTracker.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.Magenta_MagentaInjunctionTracker.sql
@@ -13,46 +13,40 @@ CREATE PROCEDURE [dbo].[Magenta_MagentaInjunctionTracker]
 AS
 
 SELECT 
-	dim_matter_header_current.master_client_code + '-' + dim_matter_header_current.master_matter_number AS [Weightmans Reference]
-	, dim_matter_worktype.work_type_name
-	, gascomp_uprn		AS [UPRN]
-	, gascomp_region AS [Region]
-	, dim_matter_header_current.matter_description			AS [Matter Description]
-	, CAST(dim_matter_header_current.date_opened_practice_management AS DATE)		AS [Date Opened]
-	, CAST(dim_matter_header_current.date_closed_practice_management AS DATE)		AS [Date Closed]
-	, dim_matter_header_current.matter_owner_full_name		AS [Case Manager]
-	, CAST(dim_detail_claim.gascomp_lba_date_upload	AS DATE)		AS [LBA Date Upload]
-	, CAST(dim_detail_claim.gascomp_lba_expiry_date AS DATE)		AS [LBA Expiry Date]
-	, CAST(dim_detail_claim.gascomp_injunction_application_date AS DATE)	AS [Injunction Application Date]
-	, dim_detail_claim.gascomp_injunction_type		AS [Injunction Type]
-	, CAST(dim_detail_claim.gascomp_hearing_date AS DATE)			AS [Hearing Date]
-	, CAST(dim_detail_claim.gascomp_date_order_served AS DATE)		AS [Date Order Served]
-	, CAST(dim_detail_claim.gascomp_injunction_service_date AS DATE)		AS [Injunction Service Date]
-	, dim_detail_claim.gascomp_comments				AS [Comments]
-	, fact_finance_summary.total_amount_bill_non_comp		AS [Total Billed]
-	, fact_finance_summary.defence_costs_billed			AS [Revenue]
-	, fact_finance_summary.disbursements_billed		AS [Disbursements]
-	, fact_finance_summary.vat_billed			AS [VAT]
-	, CASE
-		WHEN (fact_matter_summary_current.last_bill_date) = '1753-01-01' THEN
-			NULL
-		ELSE
-            CAST(fact_matter_summary_current.last_bill_date AS DATE)
-	  END													AS [Last Bill Date],
-
-	[Expiry of Gas Certificate]  =  CAST(dim_detail_claim.[gascomp_expiry_of_gas_certificate] AS DATE),
-	[Date Access Obtained] = dim_detail_claim.[gascomp_date_access_obtained], 
-	[Current Status]  = dim_detail_claim.[gascomp_current_status],
-	[Reason over 3 months] = dim_detail_claim.[gascomp_reason_over_three_months],
-
-	[Completed_Ongoing_Flag] =CASE WHEN dim_detail_claim.[gascomp_date_access_obtained] IS NOT NULL THEN 'Completed' ELSE 'Ongoing' END,
-	[Tenant's Name] = TRIM(REPLACE(REPLACE(REPLACE(SUBSTRING(REPLACE(REPLACE(REPLACE(REPLACE(dim_matter_header_current.matter_description, 'Kerrie-Louise', 'Kerrie Louise'), 'Wilkes-Ryan', 'Wilkes Ryan'), 'Abdel-Salam', 'Abdel Salam'), 'Hannah-Martin', 'Hannah Martin'), CHARINDEX('GAS  ', REPLACE(REPLACE(REPLACE(REPLACE(dim_matter_header_current.matter_description, 'Kerrie-Louise', 'Kerrie Louise'), 'Wilkes-Ryan', 'Wilkes Ryan'), 'Abdel-Salam', 'Abdel Salam'), 'Hannah-Martin', 'Hannah Martin')), CHARINDEX('-',REPLACE(REPLACE(REPLACE(REPLACE(dim_matter_header_current.matter_description, 'Kerrie-Louise', 'Kerrie Louise'), 'Wilkes-Ryan', 'Wilkes Ryan'), 'Abdel-Salam', 'Abdel Salam'), 'Hannah-Martin', 'Hannah Martin')) - CHARINDEX('GAS  ', REPLACE(REPLACE(REPLACE(REPLACE(dim_matter_header_current.matter_description, 'Kerrie-Louise', 'Kerrie Louise'), 'Wilkes-Ryan', 'Wilkes Ryan'), 'Abdel-Salam', 'Abdel Salam'), 'Hannah-Martin', 'Hannah Martin')) + Len('-')), 'GAS  ', ''), 'GAS ', ''), '-', '')),
-	[Tenant's Address] = CASE WHEN matter_description LIKE '%-%' THEN  TRIM(',' FROM REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(matter_description, TRIM(REPLACE(REPLACE(REPLACE(SUBSTRING(dim_matter_header_current.matter_description, CHARINDEX('GAS  ', dim_matter_header_current.matter_description), CHARINDEX('-',dim_matter_header_current.matter_description) - CHARINDEX('GAS  ', dim_matter_header_current.matter_description) + Len('-')), 'GAS  ', ''), 'GAS ', ''), '-', '')), ''), RIGHT(dim_matter_header_current.matter_description,CHARINDEX(',',REVERSE(dim_matter_header_current.matter_description))-1), ''), 'GAS  - ', ''), 'GAS -', ''), 'Martin - ', ''), 'Louise Darby - ', ''), 'Salam - ', ''), 'Ryan - ', '')) ELSE matter_description END
-	,[Tenant's Postcode] = right(dim_matter_header_current.matter_description,charindex(' ',reverse(dim_matter_header_current.matter_description))+4)--RIGHT(dim_matter_header_current.matter_description,CHARINDEX(',',REVERSE(dim_matter_header_current.matter_description))-1) 
-	,matter_description
-  ,ms_fileid
-  ,instruction_type
-  
+	 [Weightmans Reference] = dim_matter_header_current.master_client_code + '-' + dim_matter_header_current.master_matter_number 
+	,[UPRN] =  gascomp_uprn
+    ,[Tenant's Name] = REPLACE(REPLACE(SUBSTRING(REPLACE(matter_description,'Injunction:','Injunction') , CHARINDEX('Injunction', REPLACE(matter_description,'Injunction:','Injunction') ), CHARINDEX(':',REPLACE(matter_description,'Injunction:','Injunction') ) - CHARINDEX('Injunction', REPLACE(matter_description,'Injunction:','Injunction') ) + Len(':')), 'Injunction ',''),':','')
+	,[Tenant's Address] = REPLACE(RIGHT(matter_description,NULLIF(CHARINDEX(':',REVERSE(matter_description)),0)-1) ,ISNULL(CASE WHEN RIGHT(dim_matter_header_current.matter_description,charindex(' ',reverse(dim_matter_header_current.matter_description))+4) LIKE '%,%' THEN NULL ELSE RIGHT(dim_matter_header_current.matter_description,charindex(' ',reverse(dim_matter_header_current.matter_description))+4) END, '') , '') 
+	,[Tenant's Postcode] = CASE WHEN RIGHT(dim_matter_header_current.matter_description,charindex(' ',reverse(dim_matter_header_current.matter_description))+4) LIKE '%,%' THEN NULL ELSE RIGHT(dim_matter_header_current.matter_description,charindex(' ',reverse(dim_matter_header_current.matter_description))+4) END 	
+	,[Date Opened] = CAST(dim_matter_header_current.date_opened_practice_management AS DATE)		
+	,[Date Closed] = CAST(dim_matter_header_current.date_closed_practice_management AS DATE)		
+	,[Case Manager] = dim_matter_header_current.matter_owner_full_name		
+	,[Expiry of Gas Certificate]  =  CAST(dim_detail_claim.[gascomp_expiry_of_gas_certificate] AS DATE)
+	,[LBA Date Upload] = CAST(dim_detail_claim.gascomp_lba_date_upload	AS DATE)		 
+	,[LBA Expiry Date] = CAST(dim_detail_claim.gascomp_lba_expiry_date AS DATE)		
+	,[Injunction Application Date] = CAST(dim_detail_claim.gascomp_injunction_application_date AS DATE)	
+	,[Injunction Type] = dim_detail_claim.gascomp_injunction_type		
+	,[Hearing Date] = CAST(dim_detail_claim.gascomp_hearing_date AS DATE)			
+	,[Date Order Served] = CAST(dim_detail_claim.gascomp_date_order_served AS DATE)		
+	,[Injunction Service Date] = CAST(dim_detail_claim.gascomp_injunction_service_date AS DATE)		
+	,[Date Access Obtained] = dim_detail_claim.[gascomp_date_access_obtained]
+	,[Current Status]  = dim_detail_claim.[gascomp_current_status]
+	,[Reason over 3 months] = dim_detail_claim.[gascomp_reason_over_three_months]
+	,[Comments] = dim_detail_claim.gascomp_comments	
+	,[Magenta Instruction Type] = cboMagenInsType.cboMagenInsType -- Added on 25-03-2022 MT - Waiting on DWH Field
+	,[Total Billed] =  fact_finance_summary.total_amount_bill_non_comp		 
+	,[Revenue] = fact_finance_summary.defence_costs_billed			
+	,[Disbursements] = fact_finance_summary.disbursements_billed		
+	,[VAT] = fact_finance_summary.vat_billed			
+	,[Last Bill Date] =CASE WHEN (fact_matter_summary_current.last_bill_date) = '1753-01-01' THEN NULL ELSE CAST(fact_matter_summary_current.last_bill_date AS DATE) END													
+	,[Completed_Ongoing_Flag] =CASE WHEN dim_detail_claim.[gascomp_date_access_obtained] IS NOT NULL THEN 'Completed' ELSE 'Ongoing' END
+	,dim_matter_worktype.work_type_name
+	,ms_fileid
+    ,instruction_type
+	,[Region] =  gascomp_region 
+    ,[Matter Description] = dim_matter_header_current.matter_description	
+     ,[Gas or ELec] = CASE WHEN lower(cboMagenInsType.cboMagenInsType) LIKE '%gas%' THEN 'Gas'
+	                       WHEN lower(cboMagenInsType.cboMagenInsType) LIKE '%elec%' THEN 'Elec' END
 FROM red_dw.dbo.dim_matter_header_current
 	LEFT OUTER JOIN red_dw.dbo.dim_detail_claim
 		ON dim_detail_claim.client_code = dim_matter_header_current.client_code
@@ -72,9 +66,15 @@ FROM red_dw.dbo.dim_matter_header_current
 	ON dim_instruction_type.dim_instruction_type_key = dim_matter_header_current.dim_instruction_type_key
 	LEFT JOIN red_dw.dbo.dim_detail_outcome
 	ON dim_detail_outcome.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT JOIN ms_prod.dbo.udMICoreGeneralA
+	ON fileID = ms_fileid
+	LEFT JOIN (SELECT DISTINCT cdCode, cdDesc AS cboMagenInsType FROM  MS_PROD.dbo.udMapDetail
+JOIN ms_prod.dbo.dbCodeLookup ON txtLookupCode = cdType
+WHERE txtMSCode = 'cboMagenInsType' AND txtMSTable = 'udMICoreGeneralA') cboMagenInsType 
+ON cboMagenInsType.cdCode = udMICoreGeneralA.cboMagenInsType
 
 WHERE 1 = 1
-	AND dim_matter_header_current.reporting_exclusions = 0
+	
 	AND dim_matter_header_current.master_client_code = 'W15498'
 	AND reporting_exclusions = 0
 	AND ISNULL(matter_description, '') <> 'Ignore - opened in error'
--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.NPGPreBilling.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.NPGPreBilling.sql
@@ -22,6 +22,10 @@ GO
 
 
 
+
+
+
+
 CREATE PROCEDURE [dbo].[NPGPreBilling]
 (
 @Team AS NVARCHAR(100)
@@ -30,6 +34,52 @@ CREATE PROCEDURE [dbo].[NPGPreBilling]
 AS 
 
 BEGIN 
+
+
+IF OBJECT_ID(N'tempdb..#Fees') IS NOT NULL BEGIN DROP TABLE #Fees END
+IF OBJECT_ID(N'tempdb..#Disbs') IS NOT NULL BEGIN DROP TABLE #Disbs END
+SELECT dbfile.fileid
+,		SUM(TB.billamt) AS [Legal Costs]
+,		SUM(cbt.chrgamt) AS [VAT on Legal Costs]
+,MAX(ARD.invdate) AS LastBillDate
+INTO #Fees
+FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
+INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
+ ON ARD.matter=m.mattindex
+INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
+ ON m.mattindex=dbfile.fileextlinkid
+INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
+ ON ds_sh_ms_dbclient.clid = dbfile.clid
+INNER JOIN red_dw.dbo.ds_sh_3e_timebill TB WITH(NOLOCK)  
+ON TB.armaster = ARD.armindex
+LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT WITH(NOLOCK) 
+ON tb.timebillindex = cbt.timebill
+WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+AND ARD.arlist  IN ('Bill','BillRev')
+GROUP BY dbfile.fileid
+
+
+SELECT dbfile.fileid
+,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)=0 THEN CB.billamt ELSE 0 END) AS NonVatableDisbs
+,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)<>0 THEN CB.billamt ELSE 0 END) AS VatableDisbs
+,SUM(CBT.chrgamt) AS VatonDisbs
+INTO #Disbs
+FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
+INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
+ ON ARD.matter=m.mattindex
+INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
+ ON m.mattindex=dbfile.fileextlinkid
+INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
+ ON ds_sh_ms_dbclient.clid = dbfile.clid
+INNER JOIN red_dw.dbo.ds_sh_3e_costbill CB ON CB.armaster = ard.armindex
+	LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT ON cb.costbillindex = cbt.costbill
+WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+AND ARD.arlist  IN ('Bill','BillRev')
+AND ARD.isreversed=0
+GROUP BY dbfile.fileid
+
+
+
 
 IF @Team='All' 
 
@@ -63,60 +113,23 @@ SELECT dbFile.fileID AS [ms_fileid]
 ,ISNULL(ms_workstream.workstream,'Other') AS workstream
 ,cboNPGFileType
 ,[red_dw].[dbo].[datetimelocal](dteCompletionD) AS [Completion Date]
---,NPGBIlls.[Billed To NPG]
---,NPGBIlls.[Billed To Other]
---,NPGBIlls.[Billed To NPG Vat]
---,NPGBIlls.[Billed To Other Vat]
+,NPGBIlls.[Billed To NPG]
+,NPGBIlls.[Billed To Other]
 ,ISNULL(curTPPaying,0) AS [Third Pary Paying]
 ,cboFeeArrang
 ,curFixedFeeAmou
 ,LastBillDate
 ,usrFullName AS [Matter Owner]
+,TotalWIP.TotalWIP
+,CASE WHEN cboFeeArrang='FIXEDFEE' THEN  
+IIF(ISNULL(curFixedFeeAmou,0) - ISNULL(NPGBIlls.[Billed To NPG],0) <0 ,0,ISNULL(curFixedFeeAmou,0) - ISNULL(NPGBIlls.[Billed To NPG],0))
+ELSE NULL END AS FixedFeeLeft
 FROM ms_prod.config.dbFile WITH(NOLOCK)
 INNER JOIN MS_Prod.config.dbClient WITH(NOLOCK)
  ON dbClient.clID = dbFile.clID
-LEFT OUTER JOIN (
-SELECT dbfile.fileid
-,		SUM(TB.billamt) AS [Legal Costs]
-,		SUM(cbt.chrgamt) AS [VAT on Legal Costs]
-,MAX(ARD.invdate) AS LastBillDate
-FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
-INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
- ON ARD.matter=m.mattindex
-INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
- ON m.mattindex=dbfile.fileextlinkid
-INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
- ON ds_sh_ms_dbclient.clid = dbfile.clid
-INNER JOIN red_dw.dbo.ds_sh_3e_timebill TB WITH(NOLOCK)  
-ON TB.armaster = ARD.armindex
-LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT WITH(NOLOCK) 
-ON tb.timebillindex = cbt.timebill
-WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
-AND ARD.arlist  IN ('Bill','BillRev')
-GROUP BY dbfile.fileid
-) AS Fees
+LEFT OUTER JOIN #Fees AS Fees
  ON  Fees.fileid = dbFile.fileID
-
-LEFT OUTER JOIN 
-(
-SELECT dbfile.fileid
-,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)=0 THEN CB.billamt ELSE 0 END) AS NonVatableDisbs
-,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)<>0 THEN CB.billamt ELSE 0 END) AS VatableDisbs
-,SUM(CBT.chrgamt) AS VatonDisbs
-FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
-INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
- ON ARD.matter=m.mattindex
-INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
- ON m.mattindex=dbfile.fileextlinkid
-INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
- ON ds_sh_ms_dbclient.clid = dbfile.clid
-INNER JOIN red_dw.dbo.ds_sh_3e_costbill CB ON CB.armaster = ard.armindex
-	LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT ON cb.costbillindex = cbt.costbill
-WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
-AND ARD.arlist  IN ('Bill','BillRev')
-AND ARD.isreversed=0
-GROUP BY dbfile.fileid
-) AS Disbs
+ LEFT OUTER JOIN #Disbs AS Disbs
  ON Disbs.fileid = dbFile.fileID
 LEFT OUTER JOIN MS_PROD.dbo.udMIClientNPG WITH(NOLOCK)
  ON udMIClientNPG.fileID = dbfile.fileID
@@ -153,6 +166,7 @@ GROUP BY ms_fileid
 LEFT OUTER JOIN 
 (
 SELECT ms_fileid,name,hourly_charge_rate,SUM(minutes_recorded) AS MinutesRecorded,SUM(time_charge_value)  AS WIPValue 
+,COUNT(1) AS Records
 FROM red_dw.dbo.fact_all_time_activity WITH(NOLOCK)
 INNER JOIN red_dw.dbo.dim_fed_hierarchy_history WITH(NOLOCK)
  ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_all_time_activity.dim_fed_hierarchy_history_key
@@ -163,6 +177,19 @@ AND master_client_code IN ('WB164102','W24159','WB164104','WB164106','W22559','W
 GROUP BY ms_fileid,name,hourly_charge_rate 
 ) AS WIP
  ON dbFile.fileID=WIP.ms_fileid
+LEFT OUTER JOIN 
+(
+SELECT ms_fileid,SUM(time_charge_value)  AS TotalWIP 
+FROM red_dw.dbo.fact_all_time_activity WITH(NOLOCK)
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history WITH(NOLOCK)
+ ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_all_time_activity.dim_fed_hierarchy_history_key
+INNER JOIN red_dw.dbo.dim_matter_header_current WITH(NOLOCK)
+ ON dim_matter_header_current.dim_matter_header_curr_key = fact_all_time_activity.dim_matter_header_curr_key
+WHERE dim_bill_key=0 AND isactive=1
+AND master_client_code IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+GROUP BY ms_fileid
+) AS TotalWIP
+ ON dbFile.fileID=TotalWIP.ms_fileid
 LEFT OUTER JOIN ms_prod.dbo.dbUser ON filePrincipleID=usrID
 LEFT OUTER JOIN 
 (
@@ -182,42 +209,34 @@ WHERE 1 = 1
 ) AS ms_workstream
 	ON ms_workstream.fileID = dbFile.fileID
 LEFT OUTER JOIN ms_prod.dbo.udMICoreGeneral ON udMICoreGeneral.fileID = dbFile.fileID
---LEFT OUTER JOIN (SELECT dbfile.fileid
---,SUM(CASE WHEN Payor.DisplayName IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.arfee ELSE 0 END) AS [Billed To NPG]
---,SUM(CASE WHEN Payor.DisplayName NOT IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.arfee ELSE 0 END) AS [Billed To Other]
---,SUM(CASE WHEN Payor.DisplayName IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.artax ELSE 0 END) AS [Billed To NPG Vat]
---,SUM(CASE WHEN Payor.DisplayName NOT IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.artax ELSE 0 END) AS [Billed To Other Vat]
---FROM red_dw.dbo.ds_sh_3e_ardetail AS ARDetail WITH(NOLOCK) 
---INNER JOIN red_dw.dbo.ds_sh_3e_matter AS Matter
--- ON ARDetail.matter=Matter.mattindex
---INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
--- ON matter.mattindex=dbfile.fileextlinkid
---INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
--- ON ds_sh_ms_dbclient.clid = dbfile.clid
---LEFT OUTER JOIN te_3e_prod.dbo.Payor
--- ON ARDetail.payor=PayorIndex
--- WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
---AND arlist  IN ('Bill','BillRev')
+LEFT OUTER JOIN (
+SELECT dbfile.fileid
+,SUM(CASE WHEN Payor.DisplayName IN 
+('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
+,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
+,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
+) THEN ARDetail.arfee ELSE 0 END) AS [Billed To NPG]
+,SUM(CASE WHEN Payor.DisplayName NOT IN 
+('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
+,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
+,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
+) THEN ARDetail.arfee ELSE 0 END) AS [Billed To Other]
 
---GROUP BY dbfile.fileid
---) AS NPGBIlls
---		  ON NPGBIlls.fileid = dbFile.fileID
+FROM red_dw.dbo.ds_sh_3e_ardetail AS ARDetail WITH(NOLOCK) 
+INNER JOIN red_dw.dbo.ds_sh_3e_matter AS Matter
+ ON ARDetail.matter=Matter.mattindex
+INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
+ ON matter.mattindex=dbfile.fileextlinkid
+INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
+ ON ds_sh_ms_dbclient.clid = dbfile.clid
+LEFT OUTER JOIN te_3e_prod.dbo.Payor
+ ON ARDetail.payor=PayorIndex
+ WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+AND arlist  IN ('Bill','BillRev','BILL')
+
+GROUP BY dbfile.fileid
+) AS NPGBIlls
+		  ON NPGBIlls.fileid = dbFile.fileID
 
 WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
 AND fileNo<>'0'
@@ -259,59 +278,23 @@ SELECT dbFile.fileID AS [ms_fileid]
 ,cboNPGFileType
 ,[red_dw].[dbo].[datetimelocal](dteCompletionD) AS [Completion Date]
 ,ISNULL(curTPPaying,0) AS [Third Pary Paying]
---,NPGBIlls.[Billed To NPG]
---,NPGBIlls.[Billed To Other]
---,NPGBIlls.[Billed To NPG Vat]
---,NPGBIlls.[Billed To Other Vat]
+,NPGBIlls.[Billed To NPG]
+,NPGBIlls.[Billed To Other]
 ,cboFeeArrang
 ,curFixedFeeAmou
 ,LastBillDate
 ,usrFullName AS [Matter Owner]
+,TotalWIP.TotalWIP
+,CASE WHEN cboFeeArrang='FIXEDFEE' THEN  
+IIF(ISNULL(curFixedFeeAmou,0) - ISNULL(NPGBIlls.[Billed To NPG],0) <0 ,0,ISNULL(curFixedFeeAmou,0) - ISNULL(NPGBIlls.[Billed To NPG],0))
+ELSE NULL END AS FixedFeeLeft
 FROM ms_prod.config.dbFile WITH(NOLOCK)
 INNER JOIN MS_Prod.config.dbClient WITH(NOLOCK)
  ON dbClient.clID = dbFile.clID
-LEFT OUTER JOIN (
-SELECT dbfile.fileid
-,		SUM(TB.billamt) AS [Legal Costs]
-,		SUM(cbt.chrgamt) AS [VAT on Legal Costs]
-,MAX(ARD.invdate) AS LastBillDate
-FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
-INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
- ON ARD.matter=m.mattindex
-INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
- ON m.mattindex=dbfile.fileextlinkid
-INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
- ON ds_sh_ms_dbclient.clid = dbfile.clid
-INNER JOIN red_dw.dbo.ds_sh_3e_timebill TB WITH(NOLOCK)  
-ON TB.armaster = ARD.armindex
-LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT WITH(NOLOCK) 
-ON tb.timebillindex = cbt.timebill
-WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
-AND ARD.arlist  IN ('Bill','BillRev')
-GROUP BY dbfile.fileid
-) AS Fees
+LEFT OUTER JOIN #Fees AS Fees
  ON  Fees.fileid = dbFile.fileID
 
-LEFT OUTER JOIN 
-(
-SELECT dbfile.fileid
-,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)=0 THEN CB.billamt ELSE 0 END) AS NonVatableDisbs
-,SUM(CASE WHEN ISNULL(CBT.chrgamt,0)<>0 THEN CB.billamt ELSE 0 END) AS VatableDisbs
-,SUM(CBT.chrgamt) AS VatonDisbs
-FROM red_dw.dbo.ds_sh_3e_armaster ARD WITH(NOLOCK)
-INNER JOIN red_dw.dbo.ds_sh_3e_matter AS m WITH(NOLOCK)
- ON ARD.matter=m.mattindex
-INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
- ON m.mattindex=dbfile.fileextlinkid
-INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
- ON ds_sh_ms_dbclient.clid = dbfile.clid
-INNER JOIN red_dw.dbo.ds_sh_3e_costbill CB ON CB.armaster = ard.armindex
-	LEFT JOIN red_dw.dbo.ds_sh_3e_chrgbilltax CBT ON cb.costbillindex = cbt.costbill
-WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
-AND ARD.arlist  IN ('Bill','BillRev')
-AND ARD.isreversed=0
-GROUP BY dbfile.fileid
-) AS Disbs
+LEFT OUTER JOIN #Disbs AS Disbs
  ON Disbs.fileid = dbFile.fileID
 LEFT OUTER JOIN MS_PROD.dbo.udMIClientNPG WITH(NOLOCK)
  ON udMIClientNPG.fileID = dbfile.fileID
@@ -348,6 +331,7 @@ GROUP BY ms_fileid
 LEFT OUTER JOIN 
 (
 SELECT ms_fileid,name,hourly_charge_rate,SUM(minutes_recorded) AS MinutesRecorded,SUM(time_charge_value)  AS WIPValue 
+,COUNT(1) AS Records
 FROM red_dw.dbo.fact_all_time_activity WITH(NOLOCK)
 INNER JOIN red_dw.dbo.dim_fed_hierarchy_history WITH(NOLOCK)
  ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_all_time_activity.dim_fed_hierarchy_history_key
@@ -377,42 +361,48 @@ WHERE 1 = 1
 ) AS ms_workstream
 	ON ms_workstream.fileID = dbFile.fileID
 LEFT OUTER JOIN ms_prod.dbo.udMICoreGeneral ON udMICoreGeneral.fileID = dbFile.fileID
---LEFT OUTER JOIN (SELECT dbfile.fileid
---,SUM(CASE WHEN Payor.DisplayName IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.arfee ELSE 0 END) AS [Billed To NPG]
---,SUM(CASE WHEN Payor.DisplayName NOT IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.arfee ELSE 0 END) AS [Billed To Other]
---,SUM(CASE WHEN Payor.DisplayName IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.artax ELSE 0 END) AS [Billed To NPG Vat]
---,SUM(CASE WHEN Payor.DisplayName NOT IN 
---('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
---,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
---,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
---) THEN ARDetail.artax ELSE 0 END) AS [Billed To Other Vat]
---FROM red_dw.dbo.ds_sh_3e_ardetail AS ARDetail WITH(NOLOCK) 
---INNER JOIN red_dw.dbo.ds_sh_3e_matter AS Matter
--- ON ARDetail.matter=Matter.mattindex
---INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
--- ON matter.mattindex=dbfile.fileextlinkid
---INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
--- ON ds_sh_ms_dbclient.clid = dbfile.clid
---LEFT OUTER JOIN te_3e_prod.dbo.Payor
--- ON ARDetail.payor=PayorIndex
--- WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
---AND arlist  IN ('Bill','BillRev')
+LEFT OUTER JOIN (
+SELECT dbfile.fileid
+,SUM(CASE WHEN Payor.DisplayName IN 
+('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
+,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
+,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
+) THEN ARDetail.arfee ELSE 0 END) AS [Billed To NPG]
+,SUM(CASE WHEN Payor.DisplayName NOT IN 
+('Northern Electric Plc','Northern Powergrid (Northeast) Plc'
+,'Northern Powergrid (Yorkshire) Plc','Northern Powergrid Limited'
+,'Npower Yorkshire Limited','Yorkshire Electricity Distribution plc','Yorkshire Electricity Group Plc'
+) THEN ARDetail.arfee ELSE 0 END) AS [Billed To Other]
 
---GROUP BY  dbfile.fileid) AS NPGBIlls
---		  ON NPGBIlls.fileid = dbFile.fileID
-WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+FROM red_dw.dbo.ds_sh_3e_ardetail AS ARDetail WITH(NOLOCK) 
+INNER JOIN red_dw.dbo.ds_sh_3e_matter AS Matter
+ ON ARDetail.matter=Matter.mattindex
+INNER JOIN red_dw.dbo.ds_sh_ms_dbfile AS dbfile WITH(NOLOCK)
+ ON matter.mattindex=dbfile.fileextlinkid
+INNER JOIN red_dw.dbo.ds_sh_ms_dbclient WITH(NOLOCK)
+ ON ds_sh_ms_dbclient.clid = dbfile.clid
+LEFT OUTER JOIN te_3e_prod.dbo.Payor
+ ON ARDetail.payor=PayorIndex
+ WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+AND arlist  IN ('Bill','BillRev','BILL')
+
+GROUP BY dbfile.fileid
+) AS NPGBIlls
+		  ON NPGBIlls.fileid = dbFile.fileID
+LEFT OUTER JOIN 
+(
+SELECT ms_fileid,SUM(time_charge_value)  AS TotalWIP 
+FROM red_dw.dbo.fact_all_time_activity WITH(NOLOCK)
+INNER JOIN red_dw.dbo.dim_fed_hierarchy_history WITH(NOLOCK)
+ ON dim_fed_hierarchy_history.dim_fed_hierarchy_history_key = fact_all_time_activity.dim_fed_hierarchy_history_key
+INNER JOIN red_dw.dbo.dim_matter_header_current WITH(NOLOCK)
+ ON dim_matter_header_current.dim_matter_header_curr_key = fact_all_time_activity.dim_matter_header_curr_key
+WHERE dim_bill_key=0 AND isactive=1
+AND master_client_code IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
+GROUP BY ms_fileid
+) AS TotalWIP
+ ON dbFile.fileID=TotalWIP.ms_fileid
+ WHERE clno IN ('WB164102','W24159','WB164104','WB164106','W22559','WB170376','WB165103')
 AND fileNo<>'0'
 AND fileClosed IS NULL
 AND cboNPGFileType=@Team
