779347bf1be3bf9335b9486711b3041aec76725b
6237 <6237@sbc.tfs.local>
2022-06-15 13:18:41 +0000

    git-tfs-id: [http://svr-liv-tfs-02:8080/tfs/DefaultCollection]$/Reporting/Databases;C18222

--- Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.EPIMatterListing.sql
+++ Reporting - Live - SVR-LIV-DW-01/Stored Procedures/dbo.EPIMatterListing.sql
@@ -6,150 +6,146 @@ GO
 CREATE PROCEDURE [dbo].[EPIMatterListing]
 AS
 BEGIN
+
+
+DROP TABLE IF EXISTS #last_bill_date
+DROP TABLE IF EXISTS #chargeable_hours
+
 SELECT 
-dim_matter_header_current.client_code AS [Client Number]
-,dim_matter_header_current.matter_number AS [Matter Number]
-,master_client_code AS [MatterSphere Client Number]
-,master_matter_number AS [MatterSphere Matter Number]
-,matter_description AS [Matter Description]
-,dim_detail_practice_area.[case_description] AS [Case Description]
-,name AS [Case Manager]
-,hierarchylevel4hist AS [Team]
-,work_type_name AS [Work Type]
-,dim_matter_header_current.date_opened_case_management AS [Date Opened]
-,dim_matter_header_current.date_closed_practice_management AS [Date Closed]
-,client_group_name AS [Client Group]
-,client_name AS [Client Name]
-,fact_finance_summary.[fixed_fee_amount] [Fixed Fee Amount]
-,dim_detail_finance.[output_wip_fee_arrangement] [Fee Arrangement]
-,fact_finance_summary.[revenue_estimate_net_of_vat] [Revenue Estimate]
-,dim_detail_practice_area.[primary_case_classification] AS [Primary Case Classification]
-,dim_detail_practice_area.[secondary_case_classification] AS [Secondary Case Classification]
-,dim_detail_client.[emp_case_classification] AS [EMP Case Classification]
-,dim_detail_practice_area.[emp_claimant_represented] AS [Claimant represented?]
-,dim_detail_client.[emp_claimants_place_of_work] AS [Claimant's Place of Work]
-,dim_detail_client.[emp_rmg_sensitive_case] AS [Sensitive case]
-,dim_detail_practice_area.[ec_expiry] AS [EC Expiry]
-,dim_detail_advice.[ec_outcome] AS [EC Outcome]
-,dim_detail_advice.[ec_status] AS [EC Status]
-,dim_detail_advice.[et_claim] AS [ET Claim?]
-,dim_detail_core_details.[emp_litigatednonlitigated] AS [Litigated/Non-Litigated]
-,dim_detail_practice_area.[emp_present_position] AS [Present position]
-,dim_detail_practice_area.[date_et3_due] AS [Date ET3 due]
-,dim_detail_practice_area.[emp_prospects_of_success] AS [Prospects of success]
-,fact_detail_reserve_detail.[potential_compensation] AS [Potential compensation/pension loss]
-,dim_detail_court.[emp_date_of_preliminary_hearing_case_management] AS [Date of preliminary hearing (case management)]
-,dim_detail_court.[emp_date_of_preliminary_hearing_jurisdictionprospects] AS [Date of preliminary hearing (jurisdiction/prospects)]
-,dim_detail_court.[emp_date_of_final_hearing] AS [Date of final hearing]
-,dim_detail_practice_area.[who_was_the_advocate_at_the_hearing] AS [Advocate at hearing]
-,dim_detail_court.[length_of_hearing] AS [Length of hearing]
-,dim_detail_court.[location_of_hearing] AS [Location of hearing]
-,dim_detail_practice_area.[emp_outcome] AS [Outcome]
-,dim_detail_practice_area.[emp_stage_of_outcome] AS [Stage of outcome]
-,dim_detail_outcome.[date_claim_concluded] AS [Date claim concluded]
-,fact_detail_paid_detail.[actual_compensation] AS [Actual compensation]
-,dim_detail_practice_area.[date_remedy_hearing] AS [Date of remedy hearing]
-,dim_detail_client.[emp_what_isare_the_issues] AS [What are the issues?]
-,dim_detail_client.[emp_what_were_the_learning_points] AS [Learning Points]
-,dim_detail_client.[financial_risk] AS [Financial Risk]
-,dim_detail_client.[reputational_risk] AS [Reputational Risk]
-,dim_detail_client.[case_prospects] AS [Case Prospects]
-,dim_detail_advice.[brief_description] AS [Brief Description of Advice]
-,dim_detail_advice.[issue] AS [Advice Issue]
-,dim_detail_advice.[emph_primary_issue] AS [Advice Issue 1]
-,dim_detail_advice.[secondary_issue] AS [Secondary Advice Issue]
-,dim_detail_advice.[emph_secondary_issue] AS [Secondary Advice Issue 1]
-,dim_detail_advice.[case_classification] AS [Advice Case Classification]
-,dim_detail_advice.[category_of_advice] AS [Category of Advice]
-,dim_detail_advice.[name_of_caller] AS [Caller Name]
-,dim_detail_advice.[name_of_caller] AS [Caller Name 1]
-,dim_detail_advice.[job_title_of_caller_emp] AS [Caller Job Title]
-,dim_detail_advice.[site] AS [Site]
-,dim_detail_advice.[region] AS [Region]
-,dim_detail_advice.[geography] AS [Geography]
-,dim_detail_advice.[name_of_employee] AS [Employee Name]
-,dim_detail_advice.[job_title_of_employee] AS [Employee Job Title]
-,dim_detail_advice.[job_title_of_employee] AS [Employee Job Title 1]
-,dim_detail_advice.[workplace_postcode] AS [Workplace Postcode]
-,dim_detail_advice.[employment_start_date] AS [Employment Start Date]
-,dim_detail_advice.[risk] AS [Risk]
-,dim_detail_advice.[outcome] AS [Advice Outcome]
-,dim_detail_advice.[outcome_pe] AS [Advice Outcome 1 ]
-,dim_detail_advice.[status] AS [Status]
-,dim_detail_advice.[knowledge_gap] AS [Knowledge Gap]
-,dim_detail_advice.[policy_issue] AS [Policy Issue]
-,dim_detail_advice.[diversity_issue] AS [Diversity Issue]
-,dim_detail_advice.[date_last_call] AS [Date of Last Call]
-,total_amount_bill_non_comp AS [Total Billed]
-,defence_costs_billed_composite AS [Revenue]
-,disbursements_billed AS [Disbursements]
-,vat_non_comp AS [VAT]
-,wip AS [WIP]
-,fact_finance_summary.disbursement_balance AS [Unbilled disbursements]
-,last_bill_date AS [Date of last bill]
-,last_time_transaction_date AS [Date of last time posting]
-,AdvocacyTime.[Hours Recorded] AS [Advocacy Hours Recorded]
+	dim_matter_header_current.dim_matter_header_curr_key
+	, CAST(MAX(fact_bill_matter_detail_summary.bill_date) AS DATE)			AS last_bill_date
+INTO #last_bill_date
+FROM red_dw.dbo.fact_bill_matter_detail_summary
+	INNER JOIN red_dw.dbo.dim_matter_header_current
+		ON dim_matter_header_current.dim_matter_header_curr_key = fact_bill_matter_detail_summary.dim_matter_header_curr_key
+WHERE 
+	dim_matter_header_current.reporting_exclusions=0
+	AND dim_matter_header_current.department_code='0012'
+	AND (dim_matter_header_current.date_closed_practice_management IS NULL 
+		OR dim_matter_header_current.date_closed_practice_management>='2017-05-01')
+GROUP BY	
+	dim_matter_header_current.dim_matter_header_curr_key
 
+
+SELECT 
+	dim_matter_header_current.dim_matter_header_curr_key
+	, SUM(fact_billable_time_activity.minutes_recorded)			AS chargeable_hours
+INTO #chargeable_hours
+FROM red_dw.dbo.fact_billable_time_activity WITH(NOLOCK)
+	INNER JOIN red_dw.dbo.dim_matter_header_current WITH(NOLOCK) 
+		ON dim_matter_header_current.dim_matter_header_curr_key = fact_billable_time_activity.dim_matter_header_curr_key
+WHERE
+	dim_matter_header_current.reporting_exclusions=0
+	AND dim_matter_header_current.department_code='0012'
+	AND (dim_matter_header_current.date_closed_practice_management IS NULL 
+		OR dim_matter_header_current.date_closed_practice_management>='2017-05-01')
+GROUP BY 
+	dim_matter_header_current.dim_matter_header_curr_key
+
+
+
+
+SELECT 
+	COALESCE(NULLIF(dim_matter_header_current.client_group_name, ''), dim_matter_header_current.client_name)		AS [Client Name/Client Group]
+	, dim_matter_header_current.master_client_code + '/' + dim_matter_header_current.master_matter_number			AS [Mattersphere Client/Matter Number]
+	, dim_matter_header_current.matter_description				AS [Matter Description]
+	, dim_fed_hierarchy_history.name			AS [Case Manager]
+	, dim_fed_hierarchy_history.hierarchylevel4hist		AS [Team]
+	, CAST(dim_matter_header_current.date_opened_case_management AS DATE)		AS [Date Opened (MS)]
+	, CAST(dim_matter_header_current.date_closed_case_management AS DATE)		AS [Date Closed (MS)]
+	, dim_matter_worktype.work_type_name				AS [Matter Type]
+	, dim_detail_core_details.emp_litigatednonlitigated			AS [Litigated/Non-Litigated]
+	, dim_instruction_type.instruction_type				AS [Instruction Type (RMG)]
+	, COALESCE(dim_detail_practice_area.primary_case_classification, dim_detail_advice.emph_primary_issue, dim_detail_advice.lbs_issue, dim_detail_advice.issue)		AS [Primary Issue]
+	, COALESCE(dim_detail_practice_area.secondary_case_classification, dim_detail_advice.emph_secondary_issue, dim_detail_advice.lbs_secondary_issue, dim_detail_advice.secondary_issue)	AS [Secondary Issue]
+	, COALESCE(dim_detail_client.emp_case_classification, dim_detail_advice.case_classification, dim_detail_advice.tgif_classification)		AS [Case Classification]
+	, dim_detail_client.emp_rmg_sensitive_case			AS [Sensitive Case?]
+	, dim_detail_advice.policy_issue			AS [Policy Issue]
+	, dim_detail_advice.diversity_issue			AS [Diversity Issue]
+	, dim_detail_practice_area.emp_claimant_represented			AS [Claimant Represented]
+	, dim_claimant_thirdparty_involvement.claimantsols_name		AS [Claimant Solicitor (From Associate)]
+	, dim_detail_claim.dst_claimant_solicitor_firm			AS [Claimant Solicitor (Data Services)]
+	, CAST(dim_detail_advice.employment_start_date AS DATE)			AS [Employment Start Date]
+	, COALESCE(dim_detail_client.emp_claimants_place_of_work, dim_detail_advice.swissport_station, dim_detail_advice.site, dim_detail_advice.workplace_postcode)	AS [Claimant's Place of Work]
+	, COALESCE(dim_detail_practice_area.emp_present_position, dim_detail_advice.status, dim_detail_advice.lbs_status)			AS [Present Position]
+	, fact_detail_reserve_detail.potential_compensation				AS [Potential Compensation]
+	, dim_detail_client.financial_risk					AS [Financial Risk]
+	, COALESCE(dim_detail_practice_area.emp_prospects_of_success, dim_detail_client.case_prospects)		AS [Prospects of Success]
+	, dim_detail_client.reputational_risk				AS [Reputational Risk]
+	, dim_detail_advice.risk					AS [Risk]
+	, CAST(dim_detail_court.emp_date_of_final_hearing AS DATE)		AS [Date of Hearing]
+	, CAST(dim_detail_court.emp_date_of_preliminary_hearing_case_management AS DATE)			AS [Date of Preliminary Hearing]
+	, dim_detail_court.location_of_hearing		AS [Location of Hearing]
+	, dim_detail_court.length_of_hearing			AS [Length of Hearing]
+	, COALESCE(dim_detail_practice_area.emp_outcome, dim_detail_advice.outcome, dim_detail_advice.lbs_outcome, dim_detail_advice.outcome_pe)		AS [Outcome]
+	, fact_detail_paid_detail.actual_compensation		AS [Actual Compensation]
+	, CAST(dim_detail_outcome.date_claim_concluded AS DATE)			AS [Date Claim Concluded]
+	, dim_detail_practice_area.emp_stage_of_outcome				AS [Stage of Outcome]
+	, COALESCE(dim_detail_client.emp_what_were_the_learning_points, dim_detail_advice.knowledge_gap)			AS [Learning Points/Knowledge Gap]
+	, dim_detail_finance.output_wip_fee_arrangement			AS [Fee Arrangement]
+	, fact_finance_summary.fixed_fee_amount			AS [Fixed Fee Amount]
+	, dim_detail_finance.output_wip_percentage_complete			AS [Percentage Completion]
+	, fact_finance_summary.revenue_estimate_net_of_vat			AS [Revenue Estimate (Net of VAT)]
+	, fact_detail_reserve_detail.disbursements_estimate_net_of_vat		AS [Disbursements Estimate (Net of VAT)]
+	, fact_finance_summary.total_amount_bill_non_comp			AS [Total Billed (Incl. VAT)]
+	, fact_finance_summary.defence_costs_billed_composite						AS [Revenue]
+	, fact_finance_summary.disbursements_billed				AS [Disbursements]
+	, fact_finance_summary.vat_non_comp				AS [VAT]
+	, fact_finance_summary.wip				AS [WIP]
+	, fact_finance_summary.disbursement_balance			AS [Unbilled Disbursements]
+	, #last_bill_date.last_bill_date				AS [Date of Last Bill]
+	, #chargeable_hours.chargeable_hours				AS [Chargeable Hours Posted]
+	, CAST(fact_matter_summary_current.last_time_transaction_date AS DATE)			[Date of Last Time Posting]
 FROM red_dw.dbo.dim_matter_header_current
-INNER JOIN red_dw.dbo.dim_fed_hierarchy_history 
- ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
-  AND dss_current_flag='Y'
-LEFT OUTER JOIN red_dw.dbo.dim_detail_practice_area
- ON dim_detail_practice_area.client_code = dim_matter_header_current.client_code
- AND dim_detail_practice_area.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.dim_detail_client
- ON dim_detail_client.client_code = dim_matter_header_current.client_code
- AND dim_detail_client.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.dim_detail_advice
- ON dim_detail_advice.client_code = dim_matter_header_current.client_code
- AND dim_detail_advice.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.dim_detail_court
- ON dim_detail_court.client_code = dim_matter_header_current.client_code
- AND dim_detail_court.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.fact_detail_reserve_detail
- ON fact_detail_reserve_detail.client_code = dim_matter_header_current.client_code
- AND fact_detail_reserve_detail.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.dim_detail_outcome
- ON dim_detail_outcome.client_code = dim_matter_header_current.client_code
- AND dim_detail_outcome.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.fact_detail_paid_detail
- ON fact_detail_paid_detail.client_code = dim_matter_header_current.client_code
- AND fact_detail_paid_detail.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
- ON fact_finance_summary.client_code = dim_matter_header_current.client_code
- AND fact_finance_summary.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.fact_matter_summary_current
- ON fact_matter_summary_current.client_code = dim_matter_header_current.client_code
- AND fact_matter_summary_current.matter_number = dim_matter_header_current.matter_number
-LEFT OUTER JOIN red_dw.dbo.dim_matter_worktype
- ON dim_matter_worktype.dim_matter_worktype_key = dim_matter_header_current.dim_matter_worktype_key
- LEFT JOIN red_dw.dbo.dim_detail_finance ON dim_detail_finance.dim_matter_header_curr_key = dim_detail_advice.dim_matter_header_curr_key
-LEFT OUTER JOIN red_dw.dbo.dim_detail_core_details
-ON dim_detail_core_details.client_code = dim_matter_header_current.client_code
-AND dim_detail_core_details.matter_number = dim_matter_header_current.matter_number
+	INNER JOIN red_dw.dbo.dim_fed_hierarchy_history 
+		 ON fed_code=fee_earner_code COLLATE DATABASE_DEFAULT
+			AND dss_current_flag='Y'
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_practice_area
+		 ON dim_detail_practice_area.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key 
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_client
+		 ON dim_detail_client.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_advice
+		ON dim_detail_advice.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_claim
+		ON dim_detail_claim.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_court
+		ON dim_detail_court.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_instruction_type
+		ON dim_instruction_type.dim_instruction_type_key = dim_matter_header_current.dim_instruction_type_key
+	LEFT OUTER JOIN red_dw.dbo.fact_detail_reserve_detail
+		 ON fact_detail_reserve_detail.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_outcome
+		ON dim_detail_outcome.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.fact_detail_paid_detail
+		ON fact_detail_paid_detail.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.fact_finance_summary
+		ON fact_finance_summary.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.fact_matter_summary_current
+		ON fact_matter_summary_current.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_matter_worktype
+		ON dim_matter_worktype.dim_matter_worktype_key = dim_matter_header_current.dim_matter_worktype_key
+	LEFT JOIN red_dw.dbo.dim_detail_finance 
+		ON dim_detail_finance.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_detail_core_details
+		ON dim_detail_core_details.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN red_dw.dbo.dim_claimant_thirdparty_involvement
+		ON dim_claimant_thirdparty_involvement.client_code = dim_matter_header_current.client_code
+			AND dim_claimant_thirdparty_involvement.matter_number = dim_matter_header_current.matter_number
+	LEFT OUTER JOIN #last_bill_date
+		ON #last_bill_date.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+	LEFT OUTER JOIN #chargeable_hours
+		ON #chargeable_hours.dim_matter_header_curr_key = dim_matter_header_current.dim_matter_header_curr_key
+WHERE 
+	dim_matter_header_current.reporting_exclusions=0
+	AND dim_matter_header_current.department_code='0012'
+	AND (dim_matter_header_current.date_closed_practice_management IS NULL 
+		OR dim_matter_header_current.date_closed_practice_management>='2017-05-01')
 
-LEFT OUTER JOIN (SELECT fact_all_time_activity.client_code
-		, fact_all_time_activity.matter_number
-		, SUM(minutes_recorded)/60 AS [Hours Recorded] 
-FROM red_dw.dbo.fact_all_time_activity
-INNER JOIN red_dw.dbo.dim_all_time_activity
-ON dim_all_time_activity.dim_all_time_activity_key = fact_all_time_activity.dim_all_time_activity_key
-AND dim_all_time_activity.time_activity_code IN ('IC01','IC02','IC03','IC04','IC05','IC06','0021')
-LEFT OUTER JOIN red_dw.dbo.dim_matter_header_current
-ON dim_matter_header_current.dim_matter_header_curr_key = fact_all_time_activity.dim_matter_header_curr_key
-INNER JOIN red_dw.dbo.dim_department
-ON dim_department.dim_department_key = dim_matter_header_current.dim_department_key
-AND dim_department.department_code='0012'
-WHERE (dim_matter_header_current.date_closed_practice_management IS NULL 
-OR dim_matter_header_current.date_closed_practice_management>='2016-01-01')
-GROUP BY fact_all_time_activity.client_code,
-         fact_all_time_activity.matter_number) AS [AdvocacyTime]
-ON AdvocacyTime.client_code = dim_matter_header_current.client_code
-AND AdvocacyTime.matter_number = dim_matter_header_current.matter_number
 
-WHERE dim_matter_header_current.reporting_exclusions=0
-AND dim_matter_header_current.department_code='0012'
-AND (dim_matter_header_current.date_closed_practice_management IS NULL 
-OR dim_matter_header_current.date_closed_practice_management>='2016-01-01')
 END
+
+
+
+
+
 GO
